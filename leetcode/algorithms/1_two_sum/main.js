a38177878c8a47ff0073b7a26fc30cfb191c9220
hyungupark <hyungu.1215.park@gmail.com>
2024-10-01 19:21:17 +0900

    leetcode: "1_two_sum" add javascript and python and c# solution

---
+++ leetcode/algorithms/1_two_sum/main.js
@@ -0,0 +1,47 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (nums, target) {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = 1; j < nums.length; j++) {
+      if (i === j) {
+        continue;
+      }
+      if (nums[i] + nums[j] === target) {
+        return [i, j];
+      }
+    }
+  }
+};
+
+
+// Best Solution
+// Best Solution 1: Map
+var bestSolution1 = function (nums, target) {
+  let map = new Map();
+
+  for (let i = 0; i < nums.length; i++) {
+    if (map.has(target - nums[i])) {
+      return [map.get(target - nums[i]), i];
+    } else {
+      map.set(nums[i], i);
+    }
+  }
+  return [];
+};
+
+// Best Solution 2: Object
+var bestSolution2 = function (nums, target) {
+  let hash = {};
+
+  for (let i = 0; i < nums.length; i++) {
+    const n = nums[i];
+    if (hash[target - n] !== undefined) {
+      return [hash[target - n], i];
+    }
+    hash[n] = i;
+  }
+  return [];
+};
