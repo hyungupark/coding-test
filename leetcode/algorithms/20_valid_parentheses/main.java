5f799cb727e4441bb8a689e986a3a03a07852c62
hyungupark <hyungu.1215.park@gmail.com>
2024-10-14 22:30:43 +0900

    leetcode: "20_valid_parentheses" add java and python and dart solution

---
+++ leetcode/algorithms/20_valid_parentheses/main.java
@@ -0,0 +1,55 @@
+import java.util.HashMap;
+import java.util.Stack;
+
+class ValidParentheses {
+  public boolean isValid(String s) {
+    Stack<String> stack = new Stack<String>();
+
+    for (String c : s.split("")) {
+      if (c.equals("(")) {
+        stack.push(")");
+      } else if (c.equals("[")) {
+        stack.push("]");
+      } else if (c.equals("{")) {
+        stack.push("}");
+      } else {
+        if (stack.size() == 0 || !stack.pop().equals(c)) {
+          return false;
+        }
+      }
+    }
+
+    return stack.size() <= 0;
+  }
+
+
+  // Best Solution
+  public boolean bestSolution(String s) {
+    // Create hashmap to store the pairs...
+    HashMap<Character, Character> Hmap = new HashMap<Character, Character>();
+    Hmap.put(')', '(');
+    Hmap.put('}', '{');
+    Hmap.put(']', '[');
+    // Create stack data structure...
+    Stack<Character> stack = new Stack<Character>();
+    // Traverse each charater in input string...
+    for (int idx = 0; idx < s.length(); idx++) {
+      // If open parentheses are present, push it to stack...
+      if (s.charAt(idx) == '(' || s.charAt(idx) == '{' || s.charAt(idx) == '[') {
+        stack.push(s.charAt(idx));
+        continue;
+      }
+      // If the character is closing parentheses, check that the same type opening
+      // parentheses is being pushed to the stack or not...
+      // If not, we need to return false...
+      if (stack.size() == 0 || Hmap.get(s.charAt(idx)) != stack.pop()) {
+        return false;
+      }
+    }
+    // If the stack is empty, return true...
+    if (stack.size() == 0) {
+      return true;
+    }
+    return false;
+  }
+}
---
+++ leetcode/algorithms/20_valid_parentheses/main.py
@@ -0,0 +1,32 @@
+class validParentheses(object):
+    def isValid(self, s: str) -> bool:
+        stack = []
+
+        for c in s:
+            if c == "(":
+                stack.append(")")
+            elif c == "[":
+                stack.append("]")
+            elif c == "{":
+                stack.append("}")
+            else:
+                if len(stack) == 0 or stack.pop() != c:
+                    return False
+
+        return len(stack) <= 0
+
+
+    # Best Solution
+    def bestSolution(self, s: str) -> bool:
+        # 1. if it's the left bracket then we append it to the stack
+        # 2. else if it's the right bracket and the stack is empty(meaning no matching left bracket), or the left bracket doesn't match
+        # 3. finally check if the stack still contains unmatched left bracket
+        d = {"(": ")", "{": "}", "[": "]"}
+        stack = []
+        for i in s:
+            print(stack)
+            if i in d:  # 1
+                stack.append(i)
+            elif len(stack) == 0 or d[stack.pop()] != i:  # 2
+                return False
+        return len(stack) == 0  # 3
