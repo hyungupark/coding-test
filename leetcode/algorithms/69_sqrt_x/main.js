a47e4a42b912129f4daf0adf6aca08039923aec8
hyungupark <hyungu.1215.park@gmail.com>
2024-11-09 02:30:34 +0900

    leetcode: "69_sqrt_x" add c# and javascript solution

---
+++ leetcode/algorithms/69_sqrt_x/main.js
@@ -0,0 +1,62 @@
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var mySqrt = function (x) {
+  for (let i = 0; i <= x; i++) {
+    if (i * i <= x && (i + 1) * (i + 1) > x) {
+      return i;
+    }
+  }
+};
+
+
+// Best Solution
+// Best Solution 1:
+var bestSolution1 = function (x) {
+  var left = 1;
+  var right = Math.floor(x / 2) + 1;
+  var mid;
+
+  while (left <= right) {
+    mid = Math.floor((left + right) / 2);
+
+    if (mid * mid > x) {
+      right = mid - 1;
+    } else if (mid * mid < x) {
+      left = mid + 1;
+    } else {
+      return mid;
+    }
+  }
+
+  return right;
+};
+
+// Best Solution 2:
+var bestSolution2 = function (x) {
+  let result = 1;
+  while (result * result <= x) {
+    result++;
+  }
+  return result - 1;
+};
+
+// Best Solution 3:
+var bestSolution3 = function (x) {
+  let start = 0;
+  let end = x;
+  let res = 0;
+  while (start < end) {
+    res = Math.ceil((start + end) / 2); // 1.
+    if (res * res <= x && (res + 1) * (res + 1) > x) {
+      return res; // 2.
+    }
+    if (res * res < x) {
+      start = res; // 3.
+    } else {
+      end = res; // 4.
+    }
+  }
+  return res;
+};
