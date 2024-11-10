3da1bc65fc109a7a9a48fda87cd29b78a332dee0
hyungupark <hyungu.1215.park@gmail.com>
2024-11-10 17:27:49 +0900

    leetcode: "69_sqrt_x" add java and python and dart solution

---
+++ leetcode/algorithms/69_sqrt_x/main.py
@@ -0,0 +1,34 @@
+class SqrtX:
+    def mySqrt(self, x: int) -> int:
+        for i in range(x + 1):
+            if i * i <= x and (i + 1) * (i + 1) > x:
+                return i
+
+
+    # Best Solution
+    # Best Solution 1:
+    def bestSolution1(self, x: int) -> int:
+        if x == 0:
+            return 0
+        first, last = 1, x
+        while first <= last:
+            mid = first + (last - first) // 2
+            if mid == x // mid:
+                return mid
+            elif mid > x // mid:
+                last = mid - 1
+            else:
+                first = mid + 1
+        return last
+
+    # Best Solution 2:
+    def bestSolution2(self, x: int) -> int:
+        l, r = 0, x
+        while l <= r:
+            mid = l + (r - l) // 2
+            if mid * mid <= x < (mid + 1) * (mid + 1):
+                return mid
+            elif x < mid * mid:
+                r = mid - 1
+            else:
+                l = mid + 1
