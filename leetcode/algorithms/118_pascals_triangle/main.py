ea60e90a08183791422413c75c6623c4ebc839f4
hyungupark <hyungu.1215.park@gmail.com>
2024-12-16 15:53:26 +0900

    leetcode: "118_pascals_triangle" add python and dart solution

---
+++ leetcode/algorithms/118_pascals_triangle/main.py
@@ -0,0 +1,72 @@
+from typing import List
+
+
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        result = []
+        for i in range(0, numRows):
+            row = []
+            for j in range(0, i + 1):
+                if j == 0 or j == i:
+                    row.append(1)
+                else:
+                    row.append(result[i - 1][j - 1] + result[i - 1][j])
+            result.append(row)
+        return result
+
+    # Best Solution
+    # Best Solution 1: Recursion
+    def bestSolution1(self, numRows: int) -> List[List[int]]:
+        if numRows == 0:
+            return []
+        if numRows == 1:
+            return [[1]]
+
+        prevRows = self.generate(numRows - 1)
+        newRow = [1] * numRows
+
+        for i in range(1, numRows - 1):
+            newRow[i] = prevRows[-1][i - 1] + prevRows[-1][i]
+
+        prevRows.append(newRow)
+        return prevRows
+
+    # Best Solution 2: Combinatorial Formula
+    def bestSolution2(self, numRows: int) -> List[List[int]]:
+        result = []
+        if numRows == 0:
+            return result
+
+        first_row = [1]
+        result.append(first_row)
+
+        for i in range(1, numRows):
+            prev_row = result[i - 1]
+            current_row = [1]
+
+            for j in range(1, i):
+                current_row.append(prev_row[j - 1] + prev_row[j])
+
+            current_row.append(1)
+            result.append(current_row)
+
+        return result
+
+    # Best Solution 3: Dynamic Programming with 1D Array
+    def bestSolution3(self, numRows: int) -> List[List[int]]:
+        if numRows == 0:
+            return []
+        if numRows == 1:
+            return [[1]]
+
+        prev_rows = self.generate(numRows - 1)
+        prev_row = prev_rows[-1]
+        current_row = [1]
+
+        for i in range(1, numRows - 1):
+            current_row.append(prev_row[i - 1] + prev_row[i])
+
+        current_row.append(1)
+        prev_rows.append(current_row)
+
+        return prev_rows
