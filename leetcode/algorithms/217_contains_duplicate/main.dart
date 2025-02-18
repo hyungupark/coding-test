572fea6f43f50a5b8f2bcde56bf282bf1d5913a3
hyungupark <hyungu.1215.park@gmail.com>
2025-02-18 19:06:27 +0900

    leetcode: "217_contains_duplicate" add dart solution

---
+++ leetcode/algorithms/217_contains_duplicate/main.dart
@@ -0,0 +1,23 @@
+class ContainsDuplicate {
+  bool containsDuplicate(List<int> nums) {
+    Map<int, bool> numsMap = Map<int, bool>();
+    for (int i = 0; i < nums.length; i++) {
+      if (numsMap[nums[i]] == true) {
+        return true;
+      } else {
+        numsMap[nums[i]] = true;
+      }
+    }
+    return false;
+  }
+
+  // Best Solution
+  bool bestSolution(List<int> nums) {
+    Set<int> s = {};
+    for (int n in nums) {
+      if (s.contains(n)) return true;
+      s.add(n);
+    }
+    return false;
+  }
+}
