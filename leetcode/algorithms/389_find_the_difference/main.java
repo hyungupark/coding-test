96b8bc7d8e4a182b94133a193436504e6c8cba54
hyungupark <hyungu.1215.park@gmail.com>
2025-05-17 18:43:43 +0900

    leetcode: "389_find_the_difference" add java solution

---
+++ leetcode/algorithms/389_find_the_difference/main.java
@@ -0,0 +1,24 @@
+class FindTheDifference {
+    public char findTheDifference(String s, String t) {
+        for (int i = 0; i < s.length(); i++) {
+            t = t.replaceFirst(String.valueOf(s.charAt(i)), "");
+        }
+        return t.charAt(0);
+    }
+
+
+    // Solution
+    public char solution(String s, String t) {
+        char c = 0;
+
+        for (char cs : s.toCharArray()) {
+            c ^= cs;
+        }
+
+        for (char ct : t.toCharArray()) {
+            c ^= ct;
+        }
+
+        return c;
+    }
+}
