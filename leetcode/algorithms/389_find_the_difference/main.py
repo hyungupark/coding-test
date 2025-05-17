591c0bdc1c443cf969e9a38aa0fbbbce16e070f7
hyungupark <hyungu.1215.park@gmail.com>
2025-05-18 04:59:07 +0900

    leetcode: "389_find_the_difference" add python solution

---
+++ leetcode/algorithms/389_find_the_difference/main.py
@@ -0,0 +1,21 @@
+class FindTheDifference:
+    def findTheDifference(self, s: str, t: str) -> str:
+        for c in s:
+            t = t.replace(c, "", 1)
+        return t
+
+
+    # Solution
+    def solution(self, s: str, t: str) -> str:
+        c = 0
+
+        # ord is ASCII value
+        for cs in s:
+            c ^= ord(cs)
+
+        # ord is ASCII value
+        for ct in t:
+            c ^= ord(ct)
+
+        # chr = convert ASCII into character
+        return chr(c)
