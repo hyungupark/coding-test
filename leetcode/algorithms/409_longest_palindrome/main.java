510eb655878d31110423c5a9d7ed8772e5b940f3
hyungupark <hyungu.1215.park@gmail.com>
2025-06-01 04:22:41 +0900

    leetcode: "409_longest_palindrome" add java solution

---
+++ leetcode/algorithms/409_longest_palindrome/main.java
@@ -0,0 +1,72 @@
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+
+class LongestPalindrome {
+    public int longestPalindrome(String s) {
+        int result = 0;
+        Map<Character, Integer> sMap = new HashMap<Character, Integer>();
+
+        for (char c : s.toCharArray()) {
+            if (sMap.get(c) == null) {
+                sMap.put(c, 1);
+            } else {
+                result += 2;
+                sMap.remove(c);
+            }
+        }
+
+        if (sMap.size() > 0) {
+            result++;
+        }
+
+        return result;
+    }
+
+    // Solution
+    // Solution 1
+    public int solution1(String s) {
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+
+        HashSet<Character> hs = new HashSet<Character>();
+        int count = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (hs.contains(s.charAt(i))) {
+                hs.remove(s.charAt(i));
+                count++;
+            } else {
+                hs.add(s.charAt(i));
+            }
+        }
+
+        if (!hs.isEmpty()) {
+            return count * 2 + 1;
+        }
+
+        return count * 2;
+    }
+
+
+    // Solution 2
+    public int solution2(String s) {
+        int oddCount = 0;
+        Map<Character, Integer> map = new HashMap<>();
+
+        for (char ch : s.toCharArray()) {
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+            if (map.get(ch) % 2 == 1)
+                oddCount++;
+            else
+                oddCount--;
+        }
+
+        if (oddCount > 1) {
+            return s.length() - oddCount + 1;
+        }
+
+        return s.length();
+    }
+}
