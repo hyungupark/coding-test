ea1d43e58bd04dd3d03cc9505361061f50e73815
hyungupark <hyungu.1215.park@gmail.com>
2025-06-23 01:17:41 +0900

    leetcode: "455_assign_cookies" add python solution

---
+++ leetcode/algorithms/455_assign_cookies/main.py
@@ -0,0 +1,32 @@
+from typing import List
+
+
+class AssignCookies:
+    def findContentChildren(self, g: List[int], s: List[int]) -> int:
+        g = sorted(g)
+        s = sorted(s)
+
+        gi = 0
+        for sv in s:
+            if gi >= len(g):
+                break
+            if sv >= g[gi]:
+                gi += 1
+
+        return gi
+
+
+    # Solution
+    def solution(self, g: List[int], s: List[int]) -> int:
+        g.sort()
+        s.sort()
+
+        childi = 0
+        cookiei = 0
+
+        while cookiei < len(s) and childi < len(g):
+            if s[cookiei] >= g[childi]:
+                childi += 1
+            cookiei += 1
+
+        return childi
