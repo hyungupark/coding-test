d80570e645b9300fac8a7e6681798f27f71204d0
hyungupark <hyungu.1215.park@gmail.com>
2025-06-30 18:13:06 +0900

    leetcode: "461_hamming_distance" add c++ solution

---
+++ leetcode/algorithms/461_hamming_distance/main.cpp
@@ -0,0 +1,22 @@
+class HammingDistance {
+public:
+    int hammingDistance(int x, int y) {
+        int cnt = 0, n = x ^ y;
+        while (n) {
+            cnt += n & 1;
+            n >>= 1;
+        }
+        return cnt;
+    }
+
+
+    // Solution
+    int solution(int x, int y) {
+        int dist = 0, n = x ^ y;
+        while (n) {
+            ++dist;
+            n &= n - 1;
+        }
+        return dist;
+    }
+};
