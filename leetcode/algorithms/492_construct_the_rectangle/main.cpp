872da242c8488d583783d25bb0c41033206be0db
hyungupark <hyungu.1215.park@gmail.com>
2025-07-15 15:29:01 +0900

    leetcode: "492_construct_the_rectangle" add c++ solution

---
+++ leetcode/algorithms/492_construct_the_rectangle/main.cpp
@@ -0,0 +1,33 @@
+#include <vector>
+using namespace std;
+
+class ConstructTheRectangle {
+public:
+    vector<int> constructRectangle(int area) {
+        vector<int> result = {area, 1};
+        int diff = area;
+
+        for (int i = 1; i <= sqrt(area); i++) {
+            if (!(area % i)) {
+                if (area / i - i < diff) {
+                    result[0] = area / i;
+                    result[1] = i;
+                }
+            }
+        }
+
+        return result;
+    }
+
+
+    // Solution
+    vector<int> solution(int area) {
+        int w = (int)sqrt(area);
+
+        while (area % w != 0) {
+            w--;
+        }
+
+        return {area / w, w};
+    }
+};
