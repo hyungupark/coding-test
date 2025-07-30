1b9db60244b3109f68998c3c4c72bad2239e7cbb
hyungupark <hyungu.1215.park@gmail.com>
2025-07-31 01:07:11 +0900

    leetcode: "504_base_7" add javascript solution

---
+++ leetcode/algorithms/504_base_7/main.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} num
+ * @return {string}
+ */
+var convertToBase7 = function (num) {
+  return num.toString(7);
+};
+
+
+// Solution
+// Solution 1
+var solution1 = function (num) {
+  return num.toString(7);
+};
+
+// Solution 2
+var solution2 = (num) => num.toString(7);
