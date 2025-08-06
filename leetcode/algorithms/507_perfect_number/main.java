7bc842b9e1b7c3ac4dc05f2663b2aa9bc5188009
hyungupark <hyungu.1215.park@gmail.com>
2025-08-06 16:39:47 +0900

    leetcode: "507_perfect_number" add java solution

---
+++ leetcode/algorithms/507_perfect_number/main.java
@@ -0,0 +1,34 @@
+class PerfectNumber {
+    public boolean checkPerfectNumber(int num) {
+        if (num == 1) {
+            return false;
+        }
+
+        int sum = 0;
+        for (int i = 1; i < num / 2; i++) {
+            if (num % i == 0) {
+                sum += i;
+            }
+        }
+
+        return sum * 2 == num;
+    }
+
+
+    // Solution
+    public boolean solution(int num) {
+        if (num == 1) {
+            return false;
+        }
+
+        int sum = 0;
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                sum += i + num / i;
+            }
+        }
+        sum++;
+
+        return sum == num;
+    }
+}
