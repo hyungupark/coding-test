25a4e716f54f2b653a785a03a97cbadebaf5d617
hyungupark <hyungu.1215.park@gmail.com>
2025-10-12 02:30:40 +0900

    leetcode: "596_classes_more_than_5_students" add mssql solution

---
+++ leetcode/algorithms/596_classes_more_than_5_students/main.mssql.sql
@@ -0,0 +1,22 @@
+-- /* Write your T-SQL query statement below */
+SELECT class
+FROM Courses
+GROUP BY class
+HAVING COUNT(student) >= 5;
+
+
+-- /* Solution */
+-- /* Solution 1: Having */
+SELECT class
+FROM Courses
+GROUP BY class
+HAVING COUNT(student) >= 5;
+
+-- /* Solution 2: Subquery */
+SELECT class
+FROM (
+        SELECT class, COUNT(student) AS num
+        FROM courses
+        GROUP BY class
+    ) AS temp
+WHERE num >= 5;
