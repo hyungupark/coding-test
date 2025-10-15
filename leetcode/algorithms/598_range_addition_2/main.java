68291a50602b5f11ef9e2576943d070113d049b5
hyungupark <hyungu.1215.park@gmail.com>
2025-10-15 14:27:03 +0900

    leetcode: "598_range_addition_2" add java solution

---
+++ leetcode/algorithms/598_range_addition_2/main.java
@@ -0,0 +1,30 @@
+class RangeAdditionII {
+    public int maxCount(int m, int n, int[][] ops) {
+        int minM = m;
+        int minN = n;
+
+        for (int[] op : ops) {
+            minM = Math.min(minM, op[0]);
+            minN = Math.min(minN, op[1]);
+        }
+
+        return minM * minN;
+    }
+
+
+    // Solution
+    public int solution(int m, int n, int[][] ops) {
+        if (ops == null || ops.length == 0) {
+            return m * n;
+        }
+
+        int row = Integer.MAX_VALUE, col = Integer.MAX_VALUE;
+
+        for (int[] op : ops) {
+            row = Math.min(row, op[0]);
+            col = Math.min(col, op[1]);
+        }
+
+        return row * col;
+    }
+}
