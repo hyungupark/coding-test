3192b714b75009a18dcbcedf3cfbe1a61610ce17
hyungupark <hyungu.1215.park@gmail.com>
2026-03-01 15:08:33 +0900

    leetcode: "2_add_two_numbers" add javascript solution

--- leetcode/algorithms/2_add_two_numbers/main.js
+++ leetcode/algorithms/2_add_two_numbers/main.js
@@ -1,9 +1,14 @@
+function ListNode(val, next) {
+  this.val = val === undefined ? 0 : val;
+  this.next = next === undefined ? null : next;
+}
+
 /**
- * Definition for singly-linked list.
- * function ListNode(val, next) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.next = (next===undefined ? null : next)
- * }
+ * Complexities:
+ *   M - Length of `l1`
+ *   N - Length of `l2`
+ *   - Time Complexity: O(Max(M, N))
+ *   - Space Complexity: O(Max(M, N))
  */
 /**
  * @param {ListNode} l1
@@ -40,4 +45,47 @@ var addTwoNumbers = function (l1, l2) {
   }
 
   return result.next;
+};
+
+
+// Solution
+/**
+ * Complexities:
+ *   M - Length of `l1`
+ *   N - Length of `l2`
+ *   - Time Complexity: O(Max(M, N))
+ *   - Space Complexity: O(Max(M, N))
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var solution = function (l1, l2) {
+  let dummyHead = new ListNode(0);
+  let current = dummyHead;
+  let carry = 0;
+
+  while (l1 !== null || l2 !== null || carry > 0) {
+    const val1 = l1 !== null ? l1.val : 0;
+    const val2 = l2 !== null ? l2.val : 0;
+
+    const sum = val1 + val2 + carry;
+
+    carry = Math.floor(sum / 10);
+
+    const digit = sum % 10;
+
+    current.next = new ListNode(digit);
+    current = current.next;
+
+    if (l1 !== null) {
+      l1 = l1.next;
+    }
+    if (l2 !== null) {
+      l2 = l2.next;
+    }
+  }
+
+  return dummyHead.next;
 };
