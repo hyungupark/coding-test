2ca45d72a5468c751dc1fef803860b9a45fac08f
hyungupark <hyungu.1215.park@gmail.com>
2026-03-10 01:34:35 +0900

    leetcode: "9_palindrome_number" add c++ solution

--- leetcode/algorithms/9_palindrome_number/main.cpp
+++ leetcode/algorithms/9_palindrome_number/main.cpp
@@ -1,17 +1,18 @@
-class PalindromeNumber
-{
+class PalindromeNumber {
 public:
-    bool isPalindrome(int x)
-    {
-        if (x < 0)
-        {
+    /**
+     * Complexities:
+     *   - Time Complexity: O(logᴺ)
+     *   - Space Complexity: O(1)
+     */
+    bool isPalindrome(int x) {
+        if (x < 0) {
             return false;
         }
 
         int origin = x;
         long reverse = 0;
-        while (x > 0)
-        {
+        while (x > 0) {
             int res = x % 10;
             x /= 10;
             reverse = reverse * 10 + res;
@@ -19,25 +20,28 @@ public:
 
         return reverse == origin;
     }
+
+
+    // Solution
+    /**
+     * Complexities:
+     *   - Time Complexity: O(logᴺ)
+     *   - Space Complexity: O(1)
+     */
+    bool solution(int x) {
+        if (x < 0) {
+            return false;
+        }
+
+        long long reversed = 0;
+        int temp = x;
+
+        while (temp > 0) {
+            int digit = temp % 10;
+            reversed = reversed * 10 + digit;
+            temp /= 10;
+        }
+
+        return x == reversed;
+    }
 };
-
-// Best Solution
-bool bestSolution(int x)
-{
-    if (x < 0)
-    {
-        return false;
-    }
-
-    long long reversed = 0;
-    long long temp = x;
-
-    while (temp != 0)
-    {
-        int digit = temp % 10;
-        reversed = reversed * 10 + digit;
-        temp /= 10;
-    }
-
-    return (reversed == x);
-}
