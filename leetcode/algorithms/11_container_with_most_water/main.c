88e13da6a7d1f5de0b4567dd5146d08ed3d65c54
hyungupark <hyungu.1215.park@gmail.com>
2026-03-13 06:54:23 +0900

    leetcode: "11_container_with_most_water" add c solution

---
+++ leetcode/algorithms/11_container_with_most_water/main.c
@@ -0,0 +1,64 @@
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
+#define MIN(a, b) ((a) < (b) ? (a) : (b))
+
+/**
+ * Two Pointers
+ * 
+ * Complexities:
+ *   - Time Complexity: O(N)
+ *   - Space Complexity: O(1)
+ */
+int maxArea(int* height, int heightSize) {
+    int left = 0;
+    int right = heightSize - 1;
+    int maxStored = 0;
+
+    while (left < right) {
+        int leftHeight = height[left];
+        int rightHeight = height[right];
+
+        int h = MIN(leftHeight, rightHeight);
+        int store = (right - left) * h;
+
+        maxStored = MAX(maxStored, store);
+
+        if (leftHeight < rightHeight) {
+            left++;
+        } else {
+            right--;
+        }
+    }
+
+    return maxStored;
+}
+
+
+// Solution
+/**
+ * Two Pointers
+ * 
+ * Complexities:
+ *   - Time Complexity: O(N)
+ *   - Space Complexity: O(1)
+ */
+int solution(int* height, int heightSize) {
+    int left = 0;
+    int right = heightSize - 1;
+    int max_area = 0;
+
+    while (left < right) {
+        int h = MIN(height[left], height[right]);
+        int w = right - left;
+        int current_area = h * w;
+
+        max_area = MAX(max_area, current_area);
+
+        if (height[left] < height[right]) {
+            left++;
+        } else {
+            right--;
+        }
+    }
+
+    return max_area;
+}
