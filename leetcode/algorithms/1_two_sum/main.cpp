8bfaee6fda4b10bddd726965694eaed21beed690
hyungupark <hyungu.1215.park@gmail.com>
2026-05-22 06:05:13 +0900

    leetcode: "1_two_sum" add c++ solution

--- leetcode/algorithms/1_two_sum/main.cpp
+++ leetcode/algorithms/1_two_sum/main.cpp
@@ -1,18 +1,21 @@
+#include <unordered_map>
 #include <vector>
-
 using namespace std;
 
-class TwoSum
-{
+class TwoSum {
 public:
-    vector<int> twoSum(vector<int> &nums, int target)
-    {
-        for (int i = 0; i < nums.size() - 1; i++)
-        {
-            for (int j = i + 1; j < nums.size(); j++)
-            {
-                if (nums[i] + nums[j] == target)
-                {
+    /**
+     * Brute-Force
+     * 
+     * Complexities:
+     *   N - Size of `nums`
+     *   - Time Complexity: O(N²)
+     *   - Space Complexity: O(1)
+     */
+    vector<int> twoSum1(vector<int>& nums, int target) {
+        for (int i = 0; i < nums.size() - 1; i++) {
+            for (int j = i + 1; j < nums.size(); j++) {
+                if (nums[i] + nums[j] == target) {
                     return {i, j};
                 }
             }
@@ -21,66 +24,51 @@ public:
         return {};
     }
 
+    /**
+     * unordered_map
+     * 
+     * Complexities:
+     *   N - Size of `nums`
+     *   - Time Complexity: O(N)
+     *   - Space Complexity: O(N)
+     */
+    vector<int> twoSum2(vector<int>& nums, int target) {
+        unordered_map<int, int> map;
 
-    // Best Solution
-    // Best Solution 1: Brute Force
-    vector<int> bestSolution1(vector<int> &nums, int target)
-    {
-        int n = nums.size();
-        for (int i = 0; i < n - 1; i++)
-        {
-            for (int j = i + 1; j < n; j++)
-            {
-                if (nums[i] + nums[j] == target)
-                {
-                    return {i, j};
-                }
+        for (int i = 0; i < nums.size(); i++) {
+            int complement = target - nums[i];
+            if (map.count(complement)) {
+                return {i, map[complement]};
             }
+            map[nums[i]] = i;
         }
-        return {}; // No solution found
+
+        return {};
     }
 
-    // Best Solution 2: Two-pass Hash Table
-    vector<int> bestSolution2(vector<int> &nums, int target)
-    {
+
+    // Solution
+    /**
+     * One-pass Hash Table
+     * 
+     * Complexities:
+     *   N - Size of `nums`
+     *   - Time Complexity: O(N)
+     *   - Space Complexity: O(N)
+     */
+    vector<int> solution(vector<int>& nums, int target) {
         unordered_map<int, int> numMap;
-        int n = nums.size();
 
-        // Build the hash table
-        for (int i = 0; i < n; i++)
-        {
-            numMap[nums[i]] = i;
-        }
-
-        // Find the complement
-        for (int i = 0; i < n; i++)
-        {
+        for (int i = 0; i < nums.size(); i++) {
             int complement = target - nums[i];
-            if (numMap.count(complement) && numMap[complement] != i)
-            {
-                return {i, numMap[complement]};
-            }
-        }
 
-        return {}; // No solution found
-    }
-
-    // Best Solution 3: One-pass Hash Table
-    vector<int> bestSolution3(vector<int> &nums, int target)
-    {
-        unordered_map<int, int> numMap;
-        int n = nums.size();
-
-        for (int i = 0; i < n; i++)
-        {
-            int complement = target - nums[i];
-            if (numMap.count(complement))
-            {
+            if (numMap.find(complement) != numMap.end()) {
                 return {numMap[complement], i};
             }
+
             numMap[nums[i]] = i;
         }
 
-        return {}; // No solution found
+        return {};
     }
 };
