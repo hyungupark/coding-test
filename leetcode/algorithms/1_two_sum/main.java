834bd7d43bbf3d1301f5389886a14a5f30c06bf6
hyungupark <hyungu.1215.park@gmail.com>
2026-05-23 19:32:46 +0900

    leetcode: "1_two_sum" add java solution

--- leetcode/algorithms/1_two_sum/main.java
+++ leetcode/algorithms/1_two_sum/main.java
@@ -2,7 +2,15 @@ import java.util.HashMap;
 import java.util.Map;
 
 class TwoSum {
-    public int[] twoSum(int[] nums, int target) {
+    /**
+     * Brute-Force
+     *
+     * Complexities:
+     *   N - Size of `nums`
+     *   - Time Complexity: O(N²)
+     *   - Space Complexity: O(1)
+     */
+    public int[] twoSum1(int[] nums, int target) {
         for (int i = 0; i < nums.length; i++) {
             for (int j = i + 1; j < nums.length; j++) {
                 if (nums[i] + nums[j] == target) {
@@ -10,58 +18,55 @@ class TwoSum {
                 }
             }
         }
+
+        return new int[] {};
+    }
+
+    /**
+     * Hash Map
+     *
+     * Complexities:
+     *   N - Size of `nums`
+     *   - Time Complexity: O(N)
+     *   - Space Complexity: O(N)
+     */
+    public int[] twoSum2(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[] { i, map.get(complement) };
+            }
+            map.put(nums[i], i);
+        }
+
         return new int[] {};
     }
 
 
-    // Best Solution
-    // Best Solution 1: Brute Force
-    public int[] bestSolution1(int[] nums, int target) {
-        int n = nums.length;
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = i + 1; j < n; j++) {
-                if (nums[i] + nums[j] == target) {
-                    return new int[] { i, j };
-                }
-            }
-        }
-        return new int[] {}; // No solution found
-    }
+    // Solution
+    /**
+     * One-Pass Hash Table)
+     *
+     * Complexities:
+     *   N - Size of `nums`
+     *   - Time Complexity: O(N)
+     *   - Space Complexity: O(N)
+     */
+    public int[] solution(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
 
-    // Best Solution 2: Two-pass Hash Table
-    public int[] bestSolution2(int[] nums, int target) {
-        Map<Integer, Integer> numMap = new HashMap<>();
-        int n = nums.length;
-
-        // Build the hash table
-        for (int i = 0; i < n; i++) {
-            numMap.put(nums[i], i);
-        }
-
-        // Find the complement
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < nums.length; i++) {
             int complement = target - nums[i];
-            if (numMap.containsKey(complement) && numMap.get(complement) != i) {
-                return new int[] { i, numMap.get(complement) };
+
+            if (map.containsKey(complement)) {
+                return new int[] { map.get(complement), i };
             }
+
+            map.put(nums[i], i);
         }
 
-        return new int[] {}; // No solution found
-    }
-
-    // Best Solution 3: One-pass Hash Table
-    public int[] bestSolution3(int[] nums, int target) {
-        Map<Integer, Integer> numMap = new HashMap<>();
-        int n = nums.length;
-
-        for (int i = 0; i < n; i++) {
-            int complement = target - nums[i];
-            if (numMap.containsKey(complement)) {
-                return new int[] { numMap.get(complement), i };
-            }
-            numMap.put(nums[i], i);
-        }
-
-        return new int[] {}; // No solution found
+        throw new IllegalArgumentException("No two sum solution");
     }
 }
