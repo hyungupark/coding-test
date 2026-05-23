13e726e08a2137d8429c6462abadd8ba03824b20
hyungupark <hyungu.1215.park@gmail.com>
2026-05-23 19:32:13 +0900

    leetcode: "1_two_sum" add javascript solution

--- leetcode/algorithms/1_two_sum/main.js
+++ leetcode/algorithms/1_two_sum/main.js
@@ -1,47 +1,83 @@
 /**
+ * Brute-Force
+ *
+ * Complexities:
+ *   N - Size of `nums`
+ *   - Time Complexity: O(N²)
+ *   - Space Complexity: O(1)
+ */
+/**
  * @param {number[]} nums
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function (nums, target) {
+var twoSum1 = function (nums, target) {
   for (let i = 0; i < nums.length; i++) {
-    for (let j = 1; j < nums.length; j++) {
-      if (i === j) {
-        continue;
-      }
-      if (nums[i] + nums[j] === target) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] == target) {
         return [i, j];
       }
     }
   }
-};
 
-
-// Best Solution
-// Best Solution 1: Map
-var bestSolution1 = function (nums, target) {
-  let map = new Map();
-
-  for (let i = 0; i < nums.length; i++) {
-    if (map.has(target - nums[i])) {
-      return [map.get(target - nums[i]), i];
-    } else {
-      map.set(nums[i], i);
-    }
-  }
   return [];
 };
 
-// Best Solution 2: Object
-var bestSolution2 = function (nums, target) {
-  let hash = {};
+/**
+ * Hash Map
+ *
+ * Complexities:
+ *   N - Size of `nums`
+ *   - Time Complexity: O(N)
+ *   - Space Complexity: O(N)
+ */
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum2 = function (nums, target) {
+  const map = new Map();
 
   for (let i = 0; i < nums.length; i++) {
-    const n = nums[i];
-    if (hash[target - n] !== undefined) {
-      return [hash[target - n], i];
+    const complement = target - nums[i];
+    if (map[complement] != undefined) {
+      return [i, map[complement]];
     }
-    hash[n] = i;
+    map[nums[i]] = i;
   }
+
+  return [];
+};
+
+
+// Solution
+/**
+ * Hash Map
+ *
+ * Complexities:
+ *   N - Size of `nums`
+ *   - Time Complexity: O(N)
+ *   - Space Complexity: O(N)
+ */
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var solution = function (nums, target) {
+  const map = new Map();
+
+  for (let i = 0; i < nums.length; i++) {
+    const currentNum = nums[i];
+    const neededNum = target - currentNum;
+
+    if (map.has(neededNum)) {
+      return [map.get(neededNum), i];
+    }
+
+    map.set(currentNum, i);
+  }
+
   return [];
 };
