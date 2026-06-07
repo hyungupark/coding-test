11e749c925e53f030864f3da87d44624b4b9dbc2
hyungupark <hyungu.1215.park@gmail.com>
2026-06-07 21:06:23 +0900

    leetcode: "21_merge_two_sorted_lists" add c++ solution

--- leetcode/algorithms/21_merge_two_sorted_lists/main.cpp
+++ leetcode/algorithms/21_merge_two_sorted_lists/main.cpp
@@ -1,148 +1,103 @@
-#include <stdio.h>
-
-struct ListNode
-{
+struct ListNode {
     int val;
-    ListNode *next;
+    ListNode* next;
     ListNode() : val(0), next(nullptr) {}
     ListNode(int x) : val(x), next(nullptr) {}
-    ListNode(int x, ListNode *next) : val(x), next(next) {}
+    ListNode(int x, ListNode* next) : val(x), next(next) {}
 };
 
-class MergeTwoSortedLists
-{
+class MergeTwoSortedLists {
 public:
-    ListNode *mergeTwoLists(ListNode *list1, ListNode *list2)
-    {
-        ListNode result;
-        ListNode *currentNode = &result;
+    /**
+     * Iteration
+     * - Time Complexity: O(N + M)
+     * - Space Complexity: O(1)
+     */
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        ListNode dummy;
+        dummy.next = nullptr;
+        ListNode* curr_head = &dummy;
 
-        if (list1 == NULL && list2 == NULL)
-        {
-            return NULL;
-        }
-
-        while (list1 != NULL || list2 != NULL)
-        {
-            if (list1 != NULL && list2 != NULL)
-            {
-                if (list1->val > list2->val)
-                {
-                    currentNode->next = list2;
+        while (list1 || list2) {
+            if (list1 && list2) {
+                if (list1->val > list2->val) {
+                    curr_head->next = list2;
                     list2 = list2->next;
-                }
-                else
-                {
-                    currentNode->next = list1;
+                } else {
+                    curr_head->next = list1;
                     list1 = list1->next;
                 }
-            }
-            else if (list1 != NULL)
-            {
-                currentNode->next = list1;
+            } else if (list1) {
+                curr_head->next = list1;
                 list1 = list1->next;
-            }
-            else
-            {
-                currentNode->next = list2;
+            } else {
+                curr_head->next = list2;
                 list2 = list2->next;
             }
-            currentNode = currentNode->next;
+            curr_head = curr_head->next;
         }
 
-        return result.next;
+        return dummy.next;
     }
 
 
-    // Best Solution
-    // Best Solution 1: RECURSIVE
-    ListNode *bestSolution1(ListNode *list1, ListNode *list2)
-    {
-        // if list1 happen to be NULL
-        // we will simply return list2.
-        if (list1 == NULL)
-        {
-            return list2;
-        }
+    // Solution
+    /**
+     * Solution 1
+     *
+     * Iterative + Dummy Node
+     *
+     * Complexities:
+     *   N - Length of `list1`
+     *   M - Length of `list2`
+     *   - Time Complexity: O(N + M)
+     *   - Space Complexity: O(1)
+     */
+    ListNode* solution1(ListNode* list1, ListNode* list2) {
+        ListNode dummy(0);
+        ListNode* tail = &dummy;
 
-        // if list2 happen to be NULL
-        // we will simply return list1.
-        if (list2 == NULL)
-        {
-            return list1;
-        }
-
-        // if value pointend by l1 pointer is less than equal to value pointed by l2 pointer
-        // we wall call recursively l1 -> next and whole l2 list.
-        if (list1->val <= list2->val)
-        {
-            list1->next = mergeTwoLists(list1->next, list2);
-            return list1;
-        }
-        // we will call recursive l1 whole list and l2 -> next
-        else
-        {
-            list2->next = mergeTwoLists(list1, list2->next);
-            return list2;
-        }
-    }
-
-    // Best Solution 2: ITERATIVE
-    ListNode *bestSolution2(ListNode *list1, ListNode *list2)
-    {
-
-        // if list1 happen to be NULL
-        // we will simply return list2.
-        if (list1 == NULL)
-        {
-            return list2;
-        }
-
-        // if list2 happen to be NULL
-        // we will simply return list1.
-        if (list2 == NULL)
-        {
-            return list1;
-        }
-
-        ListNode *ptr = list1;
-        if (list1->val > list2->val)
-        {
-            ptr = list2;
-            list2 = list2->next;
-        }
-        else
-        {
-            list1 = list1->next;
-        }
-        ListNode *curr = ptr;
-
-        // till one of the list doesn't reaches NULL
-        while (list1 && list2)
-        {
-            if (list1->val < list2->val)
-            {
-                curr->next = list1;
+        while (list1 != nullptr && list2 != nullptr) {
+            if (list1->val <= list2->val) {
+                tail->next = list1;
                 list1 = list1->next;
-            }
-            else
-            {
-                curr->next = list2;
+            } else {
+                tail->next = list2;
                 list2 = list2->next;
             }
-            curr = curr->next;
+            tail = tail->next;
         }
 
-        // adding remaining elements of bigger list.
-        if (!list1)
-        {
-            curr->next = list2;
+        tail->next = (list1 != nullptr) ? list1 : list2;
+
+        return dummy.next;
+    }
+
+    /**
+     * Solution 2
+     *
+     * Recursive
+     *
+     * Complexities:
+     *   N - Length of `list1`
+     *   M - Length of `list2`
+     *   - Time Complexity: O(N + M)
+     *   - Space Complexity: O(N + M)
+     */
+    ListNode* solution2(ListNode* list1, ListNode* list2) {
+        if (!list1) {
+            return list2;
         }
-        else
-        {
-            curr->next = list1;
+        if (!list2) {
+            return list1;
         }
 
-        return ptr;
+        if (list1->val <= list2->val) {
+            list1->next = solution2(list1->next, list2);
+            return list1;
+        } else {
+            list2->next = solution2(list1, list2->next);
+            return list2;
+        }
     }
 };
