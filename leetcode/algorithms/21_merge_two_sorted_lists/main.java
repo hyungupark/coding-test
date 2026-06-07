dd9be4d8cecaa5b2ac7056e4fbbb17fc5afb5a15
hyungupark <hyungu.1215.park@gmail.com>
2026-06-08 01:07:50 +0900

    leetcode: "21_merge_two_sorted_lists" add java solution

--- leetcode/algorithms/21_merge_two_sorted_lists/main.java
+++ leetcode/algorithms/21_merge_two_sorted_lists/main.java
@@ -1,7 +1,5 @@
 class MergeTwoSortedLists {
-
   public class ListNode {
-
     int val;
     ListNode next;
 
@@ -18,72 +16,97 @@ class MergeTwoSortedLists {
     }
   }
 
+  /**
+   * Iterative + Dummy Node
+   *
+   * Complexities:
+   *   N - Length of `list1`
+   *   M - Length of `list2`
+   *   - Time Complexity: O(N + M)
+   *   - Space Complexity: O(1)
+   */
   public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-    ListNode result = new ListNode();
-    ListNode currentNode = result;
+    ListNode dummy = new ListNode();
+    ListNode currHead = dummy;
 
-    while (list1 != null || list2 != null) {
-      if (list1 != null && list2 != null) {
-        if (list1.val > list2.val) {
-          currentNode.next = list2;
-          list2 = list2.next;
-        } else {
-          currentNode.next = list1;
-          list1 = list1.next;
-        }
-      } else if (list1 != null) {
-        currentNode.next = list1;
+    while (list1 != null && list2 != null) {
+      if (list1.val > list2.val) {
+        currHead.next = list2;
+        list2 = list2.next;
+      } else {
+        currHead.next = list1;
+        list1 = list1.next;
+      }
+      currHead = currHead.next;
+    }
+
+    currHead.next = list1 == null ? list2 : list1;
+
+    return dummy.next;
+  }
+
+
+  // Solution
+  /**
+   * Solution 1
+   * 
+   * Iterative + Dummy Node
+   *
+   * Complexities:
+   *   N - Length of `list1`
+   *   M - Length of `list2`
+   *   - Time Complexity: O(N + M)
+   *   - Space Complexity: O(1)
+   */
+  public ListNode solution1(ListNode list1, ListNode list2) {
+    ListNode dummy = new ListNode(-1);
+    ListNode curr = dummy;
+
+    while (list1 != null && list2 != null) {
+      if (list1.val <= list2.val) {
+        curr.next = list1;
         list1 = list1.next;
       } else {
-        currentNode.next = list2;
+        curr.next = list2;
         list2 = list2.next;
       }
-      currentNode = currentNode.next;
+      curr = curr.next;
     }
 
-    return result.next;
+    if (list1 != null) {
+      curr.next = list1;
+    } else if (list2 != null) {
+      curr.next = list2;
+    }
+
+    return dummy.next;
   }
 
+  /**
+   * Solution 2
+   * 
+   * Recursive
+   *
+   * Complexities:
+   *   N - Length of `list1`
+   *   M - Length of `list2`
+   *   - Time Complexity: O(N + M)
+   *   - Space Complexity: O(N + M)
+   */
+  public ListNode solution2(ListNode list1, ListNode list2) {
+    if (list1 == null) {
+      return list2;
+    }
+    if (list2 == null) {
+      return list1;
+    }
 
-  // Best Solution
-  // Best Solution 1: recursion
-  public ListNode bestSolution1(ListNode l1, ListNode l2) {
-    if (l1 == null) {
-      return l2;
-    }
-    if (l2 == null) {
-      return l1;
-    }
-    if (l1.val < l2.val) {
-      l1.next = mergeTwoLists(l1.next, l2);
-      return l1;
+    if (list1.val <= list2.val) {
+      list1.next = solution2(list1.next, list2);
+      return list1;
     } else {
-      l2.next = mergeTwoLists(l1, l2.next);
-      return l2;
+      list2.next = solution2(list1, list2.next);
+      return list2;
     }
-  }
-
-  // Best Solution 2: iteration
-  public ListNode bestSolution2(ListNode l1, ListNode l2) {
-    ListNode head = new ListNode(0);
-    ListNode handler = head;
-    while (l1 != null && l2 != null) {
-      if (l1.val <= l2.val) {
-        handler.next = l1;
-        l1 = l1.next;
-      } else {
-        handler.next = l2;
-        l2 = l2.next;
-      }
-      handler = handler.next;
-    }
-
-    if (l1 != null) {
-      handler.next = l1;
-    } else if (l2 != null) {
-      handler.next = l2;
-    }
-
-    return head.next;
   }
 }
