f01b7cbba9d8d51f73303914f49931530ef0b46c
hyungupark <hyungu.1215.park@gmail.com>
2026-06-09 01:18:51 +0900

    leetcode: "26_remove_duplicates_from_sorted_array" add c solution

--- leetcode/algorithms/26_remove_duplicates_from_sorted_array/main.c
+++ leetcode/algorithms/26_remove_duplicates_from_sorted_array/main.c
@@ -1,59 +1,42 @@
-int removeDuplicates(int *nums, int numsSize)
-{
-    if (numsSize == 0)
-    {
-        return 0;
-    }
+/**
+ * Two Pointers
+ * 
+ * Complexities:
+ *   N - Length of `nums`
+ *   - Time Complexity: O(N)
+ *   - Space Complexity: O(1)
+ */
+int removeDuplicates(int* nums, int numsSize) {
+    int left = 0;
 
-    int j = 1;
-    for (int i = 1; i < numsSize; i++)
-    {
-        if (nums[i] != nums[j - 1])
-        {
-            nums[j] = nums[i];
-            j++;
+    for (int right = 0; right < numsSize; right++) {
+        if (nums[left] != nums[right]) {
+            nums[++left] = nums[right];
         }
     }
 
-    return j;
+    return left + 1;
 }
 
 
-// Best Solution
-// Best Solution 1:
-int bestSolution1(int *nums, int numsSize)
-{
-    if (numsSize == 0)
-    {
-        return 0;
-    }
+// Solution
+/**
+ * Two Pointers
+ * 
+ * Complexities:
+ *   N - Length of `nums`
+ *   - Time Complexity: O(N)
+ *   - Space Complexity: O(1)
+ */
+int solution(int* nums, int numsSize) {
+    int i = 0;
 
-    int k = 1; // Initialize the count of unique elements to 1
-    for (int i = 1; i < numsSize; i++)
-    {
-        if (nums[i] != nums[k - 1])
-        {
-            nums[k] = nums[i]; // Overwrite the next unique element
-            k++;
+    for (int j = 1; j < numsSize; j++) {
+        if (nums[j] != nums[i]) {
+            i++;
+            nums[i] = nums[j];
         }
     }
 
-    return k;
-}
-
-// Best Solution 2:
-int bestSolution2(int *nums, int numsSize)
-{
-    int new_size = numsSize;
-    for (int idx = 0, i = 0, j = 1; i < numsSize; idx++, j++)
-    {
-        while (j < numsSize && nums[i] == nums[j])
-        {
-            j++;
-            new_size--;
-        }
-        nums[idx] = nums[j - 1];
-        i = j;
-    }
-    return new_size;
+    return i + 1;
 }
