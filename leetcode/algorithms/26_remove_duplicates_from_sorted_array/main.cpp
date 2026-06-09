1cd8620f111c2c9492074a0605c62b6e12e5882b
hyungupark <hyungu.1215.park@gmail.com>
2026-06-10 01:07:10 +0900

    leetcode: "26_remove_duplicates_from_sorted_array" add c++ solution

--- leetcode/algorithms/26_remove_duplicates_from_sorted_array/main.cpp
+++ leetcode/algorithms/26_remove_duplicates_from_sorted_array/main.cpp
@@ -1,85 +1,88 @@
+#include <algorithm>
+#include <set>
 #include <vector>
 using namespace std;
 
-class RemoveDuplicatesFromSortedArray
-{
+class RemoveDuplicatesFromSortedArray {
 public:
-    int removeDuplicates(vector<int> &nums)
-    {
-        if (nums.size() == 0)
-        {
-            return 0;
-        }
+    /**
+     * Two Pointers
+     * 
+     * Complexities:
+     *   N - Length of `nums`
+     *   - Time Complexity: O(N)
+     *   - Space Complexity: O(1)
+     */
+    int removeDuplicates1(vector<int>& nums) {
+        int left = 0;
 
-        int j = 1;
-        for (int i = 1; i < nums.size(); i++)
-        {
-            if (nums[i] != nums[j - 1])
-            {
-                nums[j] = nums[i];
-                j++;
+        for (int right = 0; right < nums.size(); right++) {
+            if (nums[left] != nums[right]) {
+                nums[++left] = nums[right];
             }
         }
-
-        return j;
+        
+        return left + 1;
     }
 
+    /**
+     * Set
+     * 
+     * Complexities:
+     *   N - Length of `nums`
+     *   - Time Complexity: O(N * logᴺ)
+     *   - Space Complexity: O(N)
+     */
+    int removeDuplicates2(vector<int>& nums) {
+        set<int> set(begin(nums), end(nums));
 
-    // Best Solution
-    // Best Solution 1:
-    int bestSolution1(vector<int> &nums)
-    {
-        int j = 1;
-        for (int i = 1; i < nums.size(); i++)
-        {
-            if (nums[i] != nums[i - 1])
-            {
-                nums[j] = nums[i];
-                j++;
-            }
-        }
-        return j;
-    }
-
-    // Best Solution 2:
-    int bestSolution2(vector<int> &nums)
-    {
-        int count = 0;
-        for (int i = 1; i < nums.size(); i++)
-        {
-            if (nums[i] == nums[i - 1])
-                count++;
-            else
-                nums[i - count] = nums[i];
-        }
-        return nums.size() - count;
-    }
-
-    // Best Solution 3:
-    int bestSolution3(vector<int> &nums)
-    {
         int i = 0;
-        for (int n : nums)
-        {
-            if (!i || n > nums[i - 1])
-            {
-                nums[i++] = n;
-            }
+        for (auto p = set.begin(); p != set.end(); p++) {
+            int v = *p;
+            nums[i++] = v;
         }
+
         return i;
     }
 
-    // Best Solution 4:
-    int bestSolution4(vector<int> &nums)
-    {
-        int i = !nums.empty();
-        for (int n : nums)
-        {
-            if (n > nums[i - 1])
-            {
-                nums[i++] = n;
+
+    // Solution
+    /**
+     * Solution 1
+     * 
+     * Two Pointers
+     * 
+     * Complexities:
+     *   N - Length of `nums`
+     *   - Time Complexity: O(N)
+     *   - Space Complexity: O(1)
+     */
+    int solution1(vector<int>& nums) {
+        int index = 0;
+
+        for (int i = 1; i < nums.size(); ++i) {
+            if (nums[i] != nums[index]) {
+                index++;
+                nums[index] = nums[i];
             }
         }
-        return i;
+
+        return index + 1;
+    }
+
+    /**
+     * Solution 2
+     * 
+     * std::unique
+     * 
+     * Complexities:
+     *   N - Length of `nums`
+     *   - Time Complexity: O(N)
+     *   - Space Complexity: O(1)
+     */
+    int solution2(vector<int>& nums) {
+         auto last = unique(nums.begin(), nums.end());
+
+        return distance(nums.begin(), last);
     }
 };
