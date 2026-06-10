195b93cf9369db2f898a89d18829eade931aa586
hyungupark <hyungu.1215.park@gmail.com>
2026-06-11 01:04:02 +0900

    leetcode: "26_remove_duplicates_from_sorted_array" add python solution

--- leetcode/algorithms/26_remove_duplicates_from_sorted_array/main.py
+++ leetcode/algorithms/26_remove_duplicates_from_sorted_array/main.py
@@ -3,59 +3,54 @@ from typing import List
 
 
 class RemoveDuplicatesFromSortedArray:
-    def removeDuplicates(self, nums: List[int]) -> int:
-        nums[:] = sorted(list(dict.fromkeys(nums)))
-        return len(nums)
+    """
+    # Two Pointers
+    #
+    # Complexities:
+    #   N - Length of `nums`
+    #   - Time Complexity: O(N)
+    #   - Space Complexity: O(1)
+    """
+    def removeDuplicates1(self, nums: List[int]) -> int:
+        left: int = 0
+        for right in range(len(nums)):
+            if nums[left] != nums[right]:
+                left += 1
+                nums[left] = nums[right]
+            right += 1
+        return left + 1
+    
+    """
+    # Set
+    #
+    # Complexities:
+    #   N - Length of `nums`
+    #   - Time Complexity: O(N * logᴺ)
+    #   - Space Complexity: O(N)
+    """
+    def removeDuplicates2(self, nums: List[int]) -> int:
+        numsSet: List[int] = sorted(list(set(nums)))
+        i: int = 0
+        for i in range(len(numsSet)):
+            nums[i] = numsSet[i]
+        return i + 1
 
 
-    # Best Solution
-    # Best Solution 1:
-    def bestSolution1(self, nums: List[int]) -> int:
-        j = 1
-        for i in range(1, len(nums)):
-            if nums[i] != nums[i - 1]:
-                nums[j] = nums[i]
-                j += 1
-        return j
+    # Solution
+    """
+    # Two Pointers
+    #
+    # Complexities:
+    #   N - Length of `nums`
+    #   - Time Complexity: O(N)
+    #   - Space Complexity: O(1)
+    """
+    def solution(self, nums: List[int]) -> int:
+        slow = 0
 
-    # Best Solution 2: Sort in Place Using
-    def bestSolution2(self, nums: List[int]) -> int:
-        nums[:] = sorted(set(nums))
-        return len(nums)
-
-    # Best Solution 3: Two-pointers
-    def bestSolution3(self, nums: List[int]) -> int:
-        slow, fast = 0, 1
-        while fast in range(len(nums)):
-            if nums[slow] == nums[fast]:
-                fast += 1
-            else:
-                nums[slow + 1] = nums[fast]
-                fast += 1
+        for fast in range(1, len(nums)):
+            if nums[fast] != nums[slow]:
                 slow += 1
+                nums[slow] = nums[fast]
 
         return slow + 1
-
-    # Best Solution 4: Simplified two-pointers with for loops
-    def bestSolution4(self, nums: List[int]) -> int:
-        j = 0
-        for i in range(1, len(nums)):
-            if nums[j] != nums[i]:
-                j += 1
-                nums[j] = nums[i]
-        return j + 1
-
-    # Best Solution 5: Using ".pop()"
-    def bestSolution5(self, nums: List[int]) -> int:
-        i = 1
-        while i < len(nums):
-            if nums[i] == nums[i - 1]:
-                nums.pop(i)
-            else:
-                i += 1
-        return len(nums)
-
-    # Best Solution 6: Using "OrderedDict.fromkeys()"
-    def bestSolution6(self, nums: List[int]) -> int:
-        nums[:] = OrderedDict.fromkeys(nums)
-        return len(nums)
