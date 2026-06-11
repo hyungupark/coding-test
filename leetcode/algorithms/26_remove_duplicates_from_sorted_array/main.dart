6978ed13253fc6679932e7ff96601760ad1a84aa
hyungupark <hyungu.1215.park@gmail.com>
2026-06-12 01:09:06 +0900

    leetcode: "26_remove_duplicates_from_sorted_array" add dart solution

--- leetcode/algorithms/26_remove_duplicates_from_sorted_array/main.dart
+++ leetcode/algorithms/26_remove_duplicates_from_sorted_array/main.dart
@@ -1,29 +1,65 @@
 class RemoveDuplicatesFromSortedArray {
-  int removeDuplicates(List<int> nums) {
-    if (nums.length == 0) {
-      return 0;
+  /**
+   * Two Pointers
+   * 
+   * Complexities:
+   *   N - Length of `nums`
+   *   - Time Complexity: O(N)
+   *   - Space Complexity: O(1)
+   */
+  int removeDuplicates1(List<int> nums) {
+    int left = 0;
+
+    for (int right = 0; right < nums.length; right++) {
+      if (nums[left] != nums[right]) {
+        nums[++left] = nums[right];
+      }
     }
 
+    return left + 1;
+  }
+
+  /**
+   * Set
+   * 
+   * Complexities:
+   *   N - Length of `nums`
+   *   - Time Complexity: O(N * logᴺ)
+   *   - Space Complexity: O(N)
+   */
+  int removeDuplicates2(List<int> nums) {
+    List<int> numsSet = Set<int>.from(nums).toList();
+    numsSet.sort((int p, int n) => p - n);
+
+    int i = 0;
+
+    for (; i < numsSet.length; i++) {
+      nums[i] = numsSet[i];
+    }
+
+    return i;
+  }
+
+
+  // Solution
+  /**
+   * Two Pointers
+   *
+   * Complexities:
+   *   N - Length of `nums`
+   *   - Time Complexity: O(N)
+   *   - Space Complexity: O(1)
+   */
+  int solution(List<int> nums) {
     int j = 1;
+
     for (int i = 1; i < nums.length; i++) {
-      if (nums[i] != nums[j - 1]) {
+      if (nums[i] != nums[i - 1]) {
         nums[j] = nums[i];
         j++;
       }
     }
 
     return j;
-  }
-
-  // Best Solution
-  int BestSolution(List<int> nums) {
-    int k = 0;
-    for (int i = 1; i < nums.length; i++) {
-      if (nums[i - 1] != nums[i]) {
-        k++;
-        nums[k] = nums[i];
-      }
-    }
-    return k + 1;
   }
 }
