69ee256fcf5b1bc599b686d22c55591e29964ca4
hyungupark <hyungu.1215.park@gmail.com>
2026-06-13 20:20:15 +0900

    leetcode: "27_remove_element" add c++ solution

--- leetcode/algorithms/27_remove_element/main.cpp
+++ leetcode/algorithms/27_remove_element/main.cpp
@@ -1,58 +1,96 @@
+#include <algorithm>
 #include <vector>
 using namespace std;
 
-class Solution
-{
+class RemoveElement {
 public:
-    int removeElement(vector<int> &nums, int val)
-    {
+    /**
+     * Two Pointers
+     *
+     * Complexities:
+     *   N - Size of `nums`
+     *   - Time Complexity: O(N)
+     *   - Space Complexity: O(1)
+     */
+    int removeElement(vector<int>& nums, int val) {
         int i = 0;
-        for (int j = 0; j < nums.size(); j++)
-        {
-            if (nums[j] != val)
-            {
+
+        for (int j = 0; j < nums.size(); j++) {
+            if (nums[j] != val) {
                 int temp = nums[i];
                 nums[i] = nums[j];
                 nums[j] = temp;
                 i++;
             }
         }
+
         return i;
     }
 
 
-    // Best Solution
-    // Best Solution 1:
-    int bestSolution1(vector<int> &nums, int val)
-    {
-        int index = 0;
-        for (int i = 0; i < nums.size(); i++)
-        {
-            if (nums[i] != val)
-            {
-                nums[index] = nums[i];
-                index++;
+    // Solution
+    /**
+     * Solution 1
+     * 
+     * Two Pointers
+     *
+     * Complexities:
+     *   N - Size of `nums`
+     *   - Time Complexity: O(N)
+     *   - Space Complexity: O(1)
+     */
+    int solution1(vector<int>& nums, int val) {
+        int k = 0;
+
+        for (int i = 0; i < nums.size(); ++i) {
+            if (nums[i] != val) {
+                nums[k] = nums[i];
+                k++;
             }
         }
-        return index;
+
+        return k;
     }
 
-    // Best Solution 2:
-    int bestSolution2(vector<int> &nums, int val)
-    {
-        int length = nums.size(), index = 0;
-        while (index < length)
-        {
-            if (nums[index] == val)
-            {
-                nums[index] = nums[length - 1];
-                --length;
-            }
-            else
-            {
-                ++index;
+    /**
+     * Solution 2
+     * 
+     * Swap with Last
+     *
+     * Complexities:
+     *   N - Size of `nums`
+     *   - Time Complexity: O(N)
+     *   - Space Complexity: O(1)
+     */
+    int solution2(vector<int>& nums, int val) {
+        int i = 0;
+        int n = nums.size();
+
+        while (i < n) {
+            if (nums[i] == val) {
+                nums[i] = nums[n - 1];
+                n--;
+            } else {
+                i++;
             }
         }
-        return length;
+
+        return n;
+    }
+
+    /**
+     * Solution 3
+     * 
+     * algorithm - std::remove
+     *
+     * Complexities:
+     *   N - Size of `nums`
+     *   - Time Complexity: O(N)
+     *   - Space Complexity: O(1)
+     */
+    int solution3(vector<int>& nums, int val) {
+        auto newEnd = remove(nums.begin(), nums.end(), val);
+
+        return distance(nums.begin(), newEnd);
     }
 };
