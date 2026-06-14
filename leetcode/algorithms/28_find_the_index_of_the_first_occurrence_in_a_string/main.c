5d9ee9811898ad5b1e228ec4d397d74d436715a6
hyungupark <hyungu.1215.park@gmail.com>
2026-06-15 01:11:07 +0900

    leetcode: "28_find_the_index_of_the_first_occurrence_in_a_string" add c solution

--- leetcode/algorithms/28_find_the_index_of_the_first_occurrence_in_a_string/main.c
+++ leetcode/algorithms/28_find_the_index_of_the_first_occurrence_in_a_string/main.c
@@ -1,53 +1,78 @@
-#include <string.h>;
+#include <string.h>
 
-int strStr(char *haystack, char *needle)
-{
-    int needleSize = 0;
-    for (int i = 0; needle[i] != '\0'; i++)
-    {
-        needleSize++;
+/**
+ * Brute Force, Sliding Window
+ * 
+ * Complexities:
+ *   N - Length of `haystack`
+ *   M - Length of `needle`
+ *   - Time Complexity: O(N * M)
+ *   - Space Complexity: O(1)
+ */
+int strStr(char* haystack, char* needle) {
+    int start_idx = -1;
+    int haystack_idx = 0;
+    int needle_idx = 0;
+
+    while (1) {
+        if (needle[needle_idx] == '\0') {
+            return haystack_idx - needle_idx;
+        }
+
+        if (haystack[haystack_idx] == '\0') {
+            return -1;
+        }
+
+        if (haystack[haystack_idx] == needle[needle_idx]) {
+            if (start_idx == -1) {
+                start_idx = haystack_idx;
+            }
+            needle_idx++;
+            haystack_idx++;
+        } else {
+            if (start_idx > -1) {
+                haystack_idx = start_idx + 1;
+                start_idx = -1;
+            } else {
+                haystack_idx++;
+            }
+            needle_idx = 0;
+        }
     }
 
-    if (needleSize == 0)
-    {
-        return 0;
+    return -1;
+}
+
+
+// Solution
+/**
+ * Solution 1
+ * 
+ * Brute Force, Sliding Window
+ * 
+ * Complexities:
+ *   N - Length of `haystack`
+ *   M - Length of `needle`
+ *   - Time Complexity: O(N * M)
+ *   - Space Complexity: O(1)
+ */
+int solution1(char* haystack, char* needle) {
+    int h_len = strlen(haystack);
+    int n_len = strlen(needle);
+
+    if (h_len < n_len) {
+        return -1;
     }
 
-    for (int i = 0; haystack[i] != '\0'; i++)
-    {
-        if (haystack[i] == needle[0])
-        {
-            for (int j = 0; needle[j] != '\0'; j++)
-            {
-                if (haystack[i + j] != needle[j])
-                {
-                    break;
-                }
-                else if (needle[j + 1] == '\0')
-                {
-                    return i;
-                }
+    for (int i = 0; i <= h_len - n_len; i++) {
+        int j;
+        for (j = 0; j < n_len; j++) {
+            if (haystack[i + j] != needle[j]) {
+                break;
             }
         }
-    }
 
-    return -1;
-}
-
-
-// Best Solution
-// Best Solution 1:
-int bestSolution1(char *haystack, char *needle)
-{
-    if (strlen(needle) > strlen(haystack))
-    {
-        return -1;
-    }
-
-    for (int i = 0; i < strlen(haystack); i++)
-    {
-        if (!strncmp(haystack + i, needle, strlen(needle)))
-        {
+        if (j == n_len) {
             return i;
         }
     }
@@ -55,20 +80,22 @@ int bestSolution1(char *haystack, char *needle)
     return -1;
 }
 
-// Best Solution 2:
-int bestSolution2(char *haystack, char *needle)
-{
-    if (strlen(needle) > strlen(haystack))
-    {
-        return -1;
-    }
+/**
+ * Solution 2
+ * 
+ * <string.h> -> strstr
+ * 
+ * Complexities:
+ *   N - Length of `haystack`
+ *   M - Length of `needle`
+ *   - Time Complexity: O(N * M)
+ *   - Space Complexity: O(1)
+ */
+int solution2(char* haystack, char* needle) {
+    char* pos = strstr(haystack, needle);
 
-    for (int i = 0; i < strlen(haystack); i++)
-    {
-        if (!strncmp(haystack + i, needle, strlen(needle)))
-        {
-            return i;
-        }
+    if (pos != NULL) {
+        return pos - haystack;
     }
 
     return -1;
