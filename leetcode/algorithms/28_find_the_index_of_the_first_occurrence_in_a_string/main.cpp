e720d02815fb9a84f3c2a066c96f7f27dbb8d377
hyungupark <hyungu.1215.park@gmail.com>
2026-06-16 01:07:49 +0900

    leetcode: "28_find_the_index_of_the_first_occurrence_in_a_string" add c++ solution

--- leetcode/algorithms/28_find_the_index_of_the_first_occurrence_in_a_string/main.cpp
+++ leetcode/algorithms/28_find_the_index_of_the_first_occurrence_in_a_string/main.cpp
@@ -1,123 +1,100 @@
 #include <string>
+#include <vector>
 using namespace std;
 
-class FindTheIndexOfTheFirstOccurrenceInAString
-{
+class FindTheIndexOfTheFirstOccurrenceInAString {
 public:
-    int strStr(string haystack, string needle)
-    {
-        if (needle.size() == 0)
-        {
-            return 0;
+    /**
+     * Brute Force, Sliding Window
+     *
+     * Complexities:
+     *   N - Length of `haystack`
+     *   M - Length of `needle`
+     *   - Time Complexity: O(N * M)
+     *   - Space Complexity: O(1)
+     */
+    int strStr(string haystack, string needle) {
+        int haystack_len = haystack.size();
+        int needle_len = needle.size();
+
+        if (needle_len > haystack_len) {
+            return -1;
         }
 
-        for (int i = 0; i < haystack.size(); i++)
-        {
-            if (haystack[i] == needle[0])
-            {
-                for (int j = 0; j < needle.size(); j++)
-                {
-                    if (haystack[i + j] != needle[j])
-                    {
-                        break;
-                    }
-                    else if (j == needle.size() - 1)
-                    {
-                        return i;
-                    }
-                }
-            }
-        }
+        for (int haystack_idx = 0; haystack_idx < haystack_len; haystack_idx++) {
+            bool is_match = true;
 
-        return -1;
-    }
-
-
-    // Best Solution
-    // Best Solution 1:
-    int bestSolution1(string haystack, string needle)
-    {
-        int m = haystack.size(), n = needle.size();
-        for (int i = 0; i <= m - n; i++)
-        {
-            int j = 0;
-            for (; j < n; j++)
-            {
-                if (haystack[i + j] != needle[j])
-                {
+            for (int needle_idx = 0; needle_idx < needle_len; needle_idx++) {
+                if (haystack[haystack_idx + needle_idx] != needle[needle_idx]) {
+                    is_match = false;
                     break;
                 }
             }
-            if (j == n)
-            {
+
+            if (is_match) {
+                return haystack_idx;
+            }
+        }
+
+        return -1;
+    }
+
+
+    // Solution
+    /**
+     * Solution 1
+     * 
+     * Brute Force, Sliding Window 
+     *
+     * Complexities:
+     *   N - Length of `haystack`
+     *   M - Length of `needle`
+     *   - Time Complexity: O(N * M)
+     *   - Space Complexity: O(1)
+     */
+    int solution1(string haystack, string needle) {
+        int h_len = haystack.length();
+        int n_len = needle.length();
+
+        if (h_len < n_len) {
+            return -1;
+        }
+
+        for (int i = 0; i <= h_len - n_len; ++i) {
+            int j = 0;
+            
+            for (; j < n_len; ++j) {
+                if (haystack[i + j] != needle[j]) {
+                    break;
+                }
+            }
+
+            if (j == n_len) {
                 return i;
             }
         }
+
         return -1;
     }
 
-    // Best Solution 2:
-    int bestSolution2(string haystack, string needle)
-    {
-        int m = haystack.size(), n = needle.size(), p = 0;
-        while (p + n - 1 < m)
-        {
-            if (haystack.substr(p, n) == needle)
-            {
-                return p;
-            }
-            while (p++ + n - 1 < m && haystack[p] != needle[0]);
+    /**
+     * Solution 2
+     * 
+     * string::find
+     *
+     * Complexities:
+     *   N - Length of `haystack`
+     *   M - Length of `needle`
+     *   - Time Complexity: O(N * M)
+     *   - Space Complexity: O(1)
+     */
+    int solution2(string haystack, string needle) {
+        size_t found = haystack.find(needle);
+        
+        if (found != string::npos) {
+            return (int)found;
         }
+        
         return -1;
-    }
-
-    // Best Solution 3:
-    int bestSolution3(string haystack, string needle)
-    {
-        int m = haystack.size(), n = needle.size();
-        if (!n)
-        {
-            return 0;
-        }
-        vector<int> lps = kmpProcess(needle);
-        for (int i = 0, j = 0; i < m;)
-        {
-            if (haystack[i] == needle[j])
-            {
-                i++, j++;
-            }
-            if (j == n)
-            {
-                return i - j;
-            }
-            if (i < m && haystack[i] != needle[j])
-            {
-                j ? j = lps[j - 1] : i++;
-            }
-        }
-        return -1;
-    }
-
-private:
-    vector<int> kmpProcess(string needle)
-    {
-        int n = needle.size();
-        vector<int> lps(n, 0);
-        for (int i = 1, len = 0; i < n;)
-        {
-            if (needle[i] == needle[len])
-            {
-                lps[i++] = ++len;
-            }
-            else if (len)
-            {
-                len = lps[len - 1];
-            }
-            else
-            {
-                lps[i++] = 0;
-            }
-        }
-        return lps;
     }
 };
