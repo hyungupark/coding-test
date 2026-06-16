15006ef0017e12264031d247e28128956e41f421
hyungupark <hyungu.1215.park@gmail.com>
2026-06-17 01:11:55 +0900

    leetcode: "28_find_the_index_of_the_first_occurrence_in_a_string" add python solution

--- leetcode/algorithms/28_find_the_index_of_the_first_occurrence_in_a_string/main.py
+++ leetcode/algorithms/28_find_the_index_of_the_first_occurrence_in_a_string/main.py
@@ -1,91 +1,69 @@
-import sys
-
-
 class FindTheIndexOfTheFirstOccurrenceInAString:
+    """
+    # Brute Force, Sliding Window
+    #
+    # Complexities:
+    #   N - Length of `haystack`
+    #   M - Length of `needle`
+    #   - Time Complexity: O(N * M)
+    #   - Space Complexity: O(1)
+    """
     def strStr(self, haystack: str, needle: str) -> int:
-        if haystack == "":
+        haystack_len: int = len(haystack)
+        needle_len: int = len(needle)
+
+        if needle_len > haystack_len:
             return -1
-        if needle == "":
-            return 0
-        return haystack.find(needle)
 
+        for haystack_idx in range(haystack_len - needle_len + 1):
+            isMatch: bool = True
 
-    # Best Solution
-    # Best Solution 1:
-    def bestSolution1(self, haystack, needle):
-        for i in range(len(haystack) - len(needle) + 1):
-            if haystack[i : i + len(needle)] == needle:
-                return i
-        return -1
-
-    # Best Solution 2:
-    def bestSolution2(self, haystack, needle):
-        if needle == "":
-            return 0
-        for i in range(len(haystack) - len(needle) + 1):
-            for j in range(len(needle)):
-                if haystack[i + j] != needle[j]:
+            for needle_idx in range(needle_len):
+                if haystack[haystack_idx + needle_idx] != needle[needle_idx]:
+                    isMatch = False
                     break
-                if j == len(needle) - 1:
-                    return i
+
+            if isMatch:
+                return haystack_idx
+
         return -1
 
-    # Best Solution 3:
-    def bestSolution3(self, haystack, needle):
-        return haystack.find(needle)
 
-    # Best Solution 4: Rabin Karp, built-in hash, constant time
-    def bestSolution4(self, haystack, needle):
-        n, h = len(needle), len(haystack)
-        hash_n = hash(needle)
-        for i in range(h - n + 1):
-            if hash(haystack[i : i + n]) == hash_n:
-                return i
-        return -1
+    # Solution
+    """
+    # Solution 1
+    #
+    # Brute Force, Sliding Window
+    #
+    # Complexities:
+    #   N - Length of `haystack`
+    #   M - Length of `needle`
+    #   - Time Complexity: O(N * M)
+    #   - Space Complexity: O(1)
+    """
+    def solution1(self, haystack: str, needle: str) -> int:
+        n = len(haystack)
+        m = len(needle)
 
-    # Best Solution 4:
-    # Rabin Karp, numeral base for both uppercase
-    # and lowercase letters, constant time
-    def bestSolution4(self, haystack, needle):
-        def f(c):
-            return ord(c) - ord("A")
-
-        n, h, d, m = len(needle), len(haystack), ord("z") - ord("A") + 1, sys.maxint
-
-        if n > h:
+        if m > n:
             return -1
-        nd, hash_n, hash_h = d ** (n - 1), 0, 0
-        for i in range(n):
-            hash_n = (d * hash_n + f(needle[i])) % m
-            hash_h = (d * hash_h + f(haystack[i])) % m
-        if hash_n == hash_h:
-            return 0
-        for i in range(1, h - n + 1):
-            # e.g. 10*(1234-1*10**3)+5=2345
-            hash_h = (
-                d * (hash_h - f(haystack[i - 1]) * nd) + f(haystack[i + n - 1])
-            ) % m
-            if hash_n == hash_h:
+
+        for i in range(n - m + 1):
+            if haystack[i: i + m] == needle:
                 return i
+
         return -1
 
-    # Best Solution 5: KMP
-    def bestSolution5(self, haystack, needle):
-        n, h = len(needle), len(haystack)
-        i, j, nxt = 1, 0, [-1] + [0] * n
-        while i < n:
-            # calculate next array
-            if j == -1 or needle[i] == needle[j]:
-                i += 1
-                j += 1
-                nxt[i] = j
-            else:
-                j = nxt[j]
-        i = j = 0
-        while i < h and j < n:
-            if j == -1 or haystack[i] == needle[j]:
-                i += 1
-                j += 1
-            else:
-                j = nxt[j]
-        return i - j if j == n else -1
+    """
+    # Solution 2
+    #
+    # built-in: find()
+    #
+    # Complexities:
+    #   N - Length of `haystack`
+    #   M - Length of `needle`
+    #   - Time Complexity: O(N * M)
+    #   - Space Complexity: O(1)
+    """
+    def solution2(self, haystack: str, needle: str) -> int:
+        return haystack.find(needle)
