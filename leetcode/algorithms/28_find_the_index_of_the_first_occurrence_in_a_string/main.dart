b94506da3e1702226a30648dfe0ecf2a2a22a426
hyungupark <hyungu.1215.park@gmail.com>
2026-06-18 01:45:29 +0900

    leetcode: "28_find_the_index_of_the_first_occurrence_in_a_string" add dart solution

--- leetcode/algorithms/28_find_the_index_of_the_first_occurrence_in_a_string/main.dart
+++ leetcode/algorithms/28_find_the_index_of_the_first_occurrence_in_a_string/main.dart
@@ -1,19 +1,87 @@
 class FindTheIndexOfTheFirstOccurrenceInAString {
+  /**
+   * Brute Force, Sliding Window
+   *
+   * Complexities:
+   *   N - Length of `haystack`
+   *   M - Length of `needle`
+   *   - Time Complexity: O(N * M)
+   *   - Space Complexity: O(1)
+   */
   int strStr(String haystack, String needle) {
-    return haystack.indexOf(needle);
+    int haystackLen = haystack.length;
+    int needleLen = needle.length;
+
+    if (needleLen > haystackLen) {
+      return -1;
+    }
+
+    for (
+      int haystackIdx = 0;
+      haystackIdx <= haystackLen - needleLen;
+      haystackIdx++
+    ) {
+      bool isMatch = true;
+
+      for (int needleIdx = 0; needleIdx < needleLen; needleIdx++) {
+        if (haystack[haystackIdx + needleIdx] != needle[needleIdx]) {
+          isMatch = false;
+          break;
+        }
+      }
+
+      if (isMatch) {
+        return haystackIdx;
+      }
+    }
+
+    return -1;
   }
 
-  // Best Solution
-  int bestSolution(String haystack, String needle) {
-    if (needle.isEmpty || haystack.isEmpty || needle.length > haystack.length)
-      return -1;
 
-    for (int i = 0; i <= haystack.length - needle.length; i++) {
-      final String substring = haystack.substring(i, i + needle.length);
-      if (substring == needle) {
+  // Solution
+  /**
+   * Solution 1
+   * 
+   * Brute Force, Sliding Window
+   *
+   * Complexities:
+   *   N - Length of `haystack`
+   *   M - Length of `needle`
+   *   - Time Complexity: O(N * M)
+   *   - Space Complexity: O(1)
+   */
+  int solution1(String haystack, String needle) {
+    int n = haystack.length;
+    int m = needle.length;
+
+    for (int i = 0; i <= n - m; i++) {
+      int j = 0;
+
+      while (j < m && haystack[i + j] == needle[j]) {
+        j++;
+      }
+
+      if (j == m) {
         return i;
       }
     }
+
     return -1;
+  }
+
+  /**
+   * Solution 2
+   * 
+   * built-in: indexOf()
+   *
+   * Complexities:
+   *   N - Length of `haystack`
+   *   M - Length of `needle`
+   *   - Time Complexity: O(N * M)
+   *   - Space Complexity: O(1)
+   */
+  int solution2(String haystack, String needle) {
+    return haystack.indexOf(needle);
   }
 }
