2087b78c970863e51015997b2cac37bb98af4b21
hyungupark <hyungu.1215.park@gmail.com>
2026-06-18 01:45:55 +0900

    leetcode: "35_search_insert_position" add c solution

--- leetcode/algorithms/35_search_insert_position/main.c
+++ leetcode/algorithms/35_search_insert_position/main.c
@@ -1,21 +1,22 @@
-int searchInsert(int *nums, int numsSize, int target)
-{
+/**
+ * Binary Search
+ * 
+ * Complexities:
+ *   N - numsSize
+ *   - Time Complexity: O(logᴺ)
+ *   - Space Complexity: O(1)
+ */
+int searchInsert(int* nums, int numsSize, int target) {
     int left = 0;
     int right = numsSize - 1;
 
-    while (left <= right)
-    {
+    while (left <= right) {
         int mid = (left + right) / 2;
-        if (target > nums[mid])
-        {
+        if (target > nums[mid]) {
             left = mid + 1;
-        }
-        else if (target < nums[mid])
-        {
+        } else if (target < nums[mid]) {
             right = mid - 1;
-        }
-        else
-        {
+        } else {
             return mid;
         }
     }
@@ -24,39 +25,30 @@ int searchInsert(int *nums, int numsSize, int target)
 }
 
 
-// Best Solution
-// Best Solution 1: Linear Search O(n)
-int bestSolution1(int *nums, int numsSize, int target)
-{
-    for (int i = 0; i < numsSize; i++)
-    {
-        if (nums[i] >= target)
-        {
-            return i;
-        }
-    }
-    return numsSize;
-}
+// Solution
+/**
+ * Binary Search
+ * 
+ * Complexities:
+ *   N - numsSize
+ *   - Time Complexity: O(logᴺ)
+ *   - Space Complexity: O(1)
+ */
+int solution(int* nums, int numsSize, int target) {
+    int left = 0;
+    int right = numsSize - 1;
 
-// Best Solution 2: Binary Search O(log(n))
-int bestSolution2(int *nums, int numsSize, int target)
-{
-    int left = 0, right = numsSize - 1, mid;
-    while (left <= right)
-    {
-        mid = (right + left) / 2;
-        if (nums[mid] < target)
-        {
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+
+        if (nums[mid] == target) {
+            return mid;
+        } else if (nums[mid] < target) {
             left = mid + 1;
-        }
-        else if (nums[mid] > target)
-        {
+        } else {
             right = mid - 1;
         }
-        else
-        {
-            return mid;
-        }
     }
+
     return left;
 }
