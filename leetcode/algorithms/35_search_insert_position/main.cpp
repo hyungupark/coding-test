5e63dfc1a74359acb87b8efb74aa1a227b747ffa
hyungupark <hyungu.1215.park@gmail.com>
2026-06-19 01:32:06 +0900

    leetcode: "35_search_insert_position" add c++ solution

--- leetcode/algorithms/35_search_insert_position/main.cpp
+++ leetcode/algorithms/35_search_insert_position/main.cpp
@@ -1,27 +1,28 @@
+#include <algorithm>
 #include <vector>
 using namespace std;
 
-class SearchInsertPosition
-{
+class SearchInsertPosition {
 public:
-    int searchInsert(vector<int> &nums, int target)
-    {
+    /**
+     * Binary Search
+     *
+     * Complexities:
+     *   N - Size of `nums`
+     *   - Time Complexity: O(logᴺ)
+     *   - Space Complexity: O(1)
+     */
+    int searchInsert(vector<int>& nums, int target) {
         int left = 0;
         int right = nums.size() - 1;
 
-        while (left <= right)
-        {
+        while (left <= right) {
             int mid = (left + right) / 2;
-            if (target > nums[mid])
-            {
+            if (target > nums[mid]) {
                 left = mid + 1;
-            }
-            else if (target < nums[mid])
-            {
+            } else if (target < nums[mid]) {
                 right = mid - 1;
-            }
-            else
-            {
+            } else {
                 return mid;
             }
         }
@@ -30,40 +31,47 @@ public:
     }
 
 
-    // Best Solution
-    // Best Solution 1: Binary Search
-    // Time complexity: O(log(n)), Space complexity: O(1)
-    int bestSolution1(vector<int> &nums, int target)
-    {
-        int low = 0;
-        int high = nums.size();
-        int mid;
-        if (target > nums[high - 1])
-        {
-            return high;
-        }
-        while (low <= high)
-        {
-            mid = (low + high) / 2;
-            if (target > nums[mid])
-            {
-                low = mid + 1;
-            }
-            else if (target < nums[mid])
-            {
-                high = mid - 1;
-            }
-            else
-            {
+    // Solution
+    /**
+     * Solution 1
+     * 
+     * Binary Search
+     *
+     * Complexities:
+     *   N - Size of `nums`
+     *   - Time Complexity: O(logᴺ)
+     *   - Space Complexity: O(1)
+     */
+    int solution1(vector<int>& nums, int target) {
+        int left = 0;
+        int right = nums.size() - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
                 return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
             }
         }
-        return low;
+
+        return left;
     }
 
-    // Best Solution 2:
-    int bestSolution2(vector<int> &nums, int target)
-    {
+    /**
+     * Solution 2
+     * 
+     * algorithm::lower_bound
+     *
+     * Complexities:
+     *   N - Size of `nums`
+     *   - Time Complexity: O(logᴺ)
+     *   - Space Complexity: O(1)
+     */
+    int solution2(vector<int>& nums, int target) {
         return lower_bound(nums.begin(), nums.end(), target) - nums.begin();
     }
 };
