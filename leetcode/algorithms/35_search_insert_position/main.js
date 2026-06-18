8558788fdc9afafe5b80a999c5f7aaefe043065a
hyungupark <hyungu.1215.park@gmail.com>
2026-06-19 01:32:18 +0900

    leetcode: "35_search_insert_position" add javascript solution

--- leetcode/algorithms/35_search_insert_position/main.js
+++ leetcode/algorithms/35_search_insert_position/main.js
@@ -1,4 +1,12 @@
 /**
+ * Binary Search
+ *
+ * Complexities:
+ *   N - Size of `nums`
+ *   - Time Complexity: O(logᴺ)
+ *   - Space Complexity: O(1)
+ */
+/**
  * @param {number[]} nums
  * @param {number} target
  * @return {number}
@@ -12,48 +20,35 @@ var searchInsert = function (nums, target) {
 };
 
 
-// Best Solution
-// Best Solution 1:
-var bestSolution1 = function (nums, target) {
-  let lo = 0, hi = nums.length; // we might need to inseart at the end
-  while (lo < hi) {
-    // breaks if lo == hi
-    let mid = lo + Math.floor((hi - lo) / 2); // always gives the lower mid
-    if (target > nums[mid]) {
-      lo = mid + 1; // no way mid is a valid option
+// Solution
+/**
+ * Binary Search
+ *
+ * Complexities:
+ *   N - Size of `nums`
+ *   - Time Complexity: O(logᴺ)
+ *   - Space Complexity: O(1)
+ */
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var solution = function (nums, target) {
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2);
+
+    if (nums[mid] === target) {
+      return mid;
+    } else if (nums[mid] < target) {
+      left = mid + 1;
     } else {
-      hi = mid; // it might be possibe to inseart @ mid
+      right = mid - 1;
     }
   }
-  return lo;
+
+  return left;
 };
-
-// Best Solution 2:
-var bestSolution2 = function (nums, target) {
-  return binarySearch(nums, target, 0, nums.length - 1);
-};
-
-function binarySearch(array, target, start, end) {
-  // If the target is less then the very last item then insert it at that item index
-  // because anything index less then that has already been confirmed to be less then the target.
-  // Otherwise insert it at that item index + 1
-  // because any index grater then that has already been confirmed to be greater then the target
-  if (start > end) {
-    return start;
-  }
-
-  const midPoint = Math.floor((start + end) / 2);
-
-  // found target
-  if (array[midPoint] === target) {
-    return midPoint;
-  }
-  // search the left side
-  if (array[midPoint] > target) {
-    return binarySearch(array, target, start, midPoint - 1);
-  }
-  // search the right side
-  if (array[midPoint] < target) {
-    return binarySearch(array, target, midPoint + 1, end);
-  }
-}
