28947b886be7e82aae7a3a186fc11efb0ffb398e
hyungupark <hyungu.1215.park@gmail.com>
2026-06-20 01:59:13 +0900

    leetcode: "35_search_insert_position" add python solution

--- leetcode/algorithms/35_search_insert_position/main.py
+++ leetcode/algorithms/35_search_insert_position/main.py
@@ -1,9 +1,17 @@
-import math
 import bisect
+import math
 from typing import List
 
 
 class SearchInsertPosition:
+    """
+    # Binary Search
+    #
+    # Complexities:
+    #   N - Size of `nums`
+    #   - Time Complexity: O(logᴺ)
+    #   - Space Complexity: O(1)
+    """
     def searchInsert(self, nums: List[int], target: int) -> int:
         lo, hi = 0, len(nums)
         while lo < hi:
@@ -15,29 +23,42 @@ class SearchInsertPosition:
         return lo
 
 
-    # Best Solution
-    # Best Solution 1: Linear Search
-    def bestSolution1(self, nums: List[int], target: int) -> int:
-        if not nums:
-            return 0
+    # Solution
+    """
+    # Solution 1
+    #
+    # Binary Search
+    #
+    # Complexities:
+    #   N - Size of `nums`
+    #   - Time Complexity: O(logᴺ)
+    #   - Space Complexity: O(1)
+    """
+    def solution1(self, nums: List[int], target: int) -> int:
+        left = 0
+        right = len(nums) - 1
 
-        for i, num in enumerate(nums):
-            if num >= target:
-                return i
+        while left <= right:
+            mid = (left + right) // 2
 
-        return len(nums)
-
-    # Best Solution 2: Using bisect module
-    def bestSolution2(self, nums: List[int], target: int) -> int:
-        return bisect.bisect_left(nums, target)
-
-    # Best Solution 3: Binary Search By Hand
-    def bestSolution3(self, nums: List[int], target: int) -> int:
-        low, high = 0, len(nums)
-        while low < high:
-            mid = (low + high) // 2
-            if target > nums[mid]:
-                low = mid + 1
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                left = mid + 1
             else:
-                high = mid
-        return low
+                right = mid - 1
+
+        return left
+
+    """
+    # Solution 2
+    #
+    # bisect
+    #
+    # Complexities:
+    #   N - Size of `nums`
+    #   - Time Complexity: O(logᴺ)
+    #   - Space Complexity: O(1)
+    """
+    def solution2(self, nums: List[int], target: int) -> int:
+        return bisect.bisect_left(nums, target)
