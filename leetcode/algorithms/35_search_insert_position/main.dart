769cc2cdb7b56a5a3f53bff4240ab217ea68a47b
hyungupark <hyungu.1215.park@gmail.com>
2026-06-21 21:15:10 +0900

    leetcode: "35_search_insert_position" add dart solution

--- leetcode/algorithms/35_search_insert_position/main.dart
+++ leetcode/algorithms/35_search_insert_position/main.dart
@@ -1,29 +1,48 @@
 class SearchInsertPosition {
+  /**
+   * Binary Search
+   *
+   * Complexities:
+   *   N - Size of `nums`
+   *   - Time Complexity: O(logᴺ)
+   *   - Space Complexity: O(1)
+   */
   int searchInsert(List<int> nums, int target) {
     for (var i = 0; i < nums.length; i++) {
       if (nums[i] >= target) {
         return i;
       }
     }
+
     return nums.length;
   }
 
-  // Best Solution
-  int bestSolution(List<int> nums, int target) {
-    int left = 0;
-    int right = nums.length - 1;
 
-    while (right >= left) {
-      int mid = left + (right - left) ~/ 2;
+  // Solution
+  /**
+   * Binary Search
+   *
+   * Complexities:
+   *   N - Size of `nums`
+   *   - Time Complexity: O(logᴺ)
+   *   - Space Complexity: O(1)
+   */
+  int solution(List<int> nums, int target) {
+    int low = 0;
+    int high = nums.length - 1;
 
-      if (nums[mid] == target)
+    while (low <= high) {
+      int mid = low + (high - low) ~/ 2;
+
+      if (nums[mid] == target) {
         return mid;
-      else if (nums[mid] > target)
-        right = mid - 1;
-      else
-        left = mid + 1;
+      } else if (nums[mid] < target) {
+        low = mid + 1;
+      } else {
+        high = mid - 1;
+      }
     }
 
-    return left;
+    return low;
   }
 }
