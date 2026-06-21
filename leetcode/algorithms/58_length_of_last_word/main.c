c2e5627e19d8b5c44552628701eeaf9b0a45673e
hyungupark <hyungu.1215.park@gmail.com>
2026-06-21 21:18:01 +0900

    leetcode: "58_length_of_last_word" add c solution

--- leetcode/algorithms/58_length_of_last_word/main.c
+++ leetcode/algorithms/58_length_of_last_word/main.c
@@ -1,22 +1,24 @@
 #include <string.h>
 
-int lengthOfLastWord(char *s)
-{
+/**
+ * Reverse Traversal
+ * 
+ * Complexities:
+ *   N - Length of `s`
+ *   - Time Complexity: O(N)
+ *   - Space Complexity: O(1)
+ */
+int lengthOfLastWord(char* s) {
     int i = 0;
-    while (s[i] != '\0')
-    {
+    while (s[i] != '\0') {
         i++;
     }
 
     int result = 0;
-    for (; i >= 0; i--)
-    {
-        if (s[i] != ' ' && s[i] != '\0')
-        {
+    for (; i >= 0; i--) {
+        if (s[i] != ' ' && s[i] != '\0') {
             result++;
-        }
-        else if (s[i] = ' ' && result > 0)
-        {
+        } else if (s[i] = ' ' && result > 0) {
             break;
         }
     }
@@ -25,81 +27,28 @@ int lengthOfLastWord(char *s)
 }
 
 
-// Best Solution
-// Best Solution 1:
-int bestSolution1(char *s)
-{
-    int c = 0;
-    for (int i = strlen(s) - 1; i >= 0; i--)
-    {
-        if (s[i] == ' ')
-        {
-            c++;
-        }
-        else
-        {
-            break;
-        }
-    }
-    int c1 = 0;
-    for (int i = strlen(s) - c - 1; i >= 0; i--)
-    {
-        if (s[i] != ' ')
-        {
-            c1++;
-        }
-        else
-        {
-            break;
-        }
-    }
-    return c1;
-}
+// Solution
+/**
+ * Reverse Traversal
+ * 
+ * Complexities:
+ *   N - Length of `s`
+ *   - Time Complexity: O(N)
+ *   - Space Complexity: O(1)
+ */
+int solution(char* s) {
+    int len = strlen(s);
+    int length = 0;
+    int i = len - 1;
 
-// Best Solution 2:
-int bestSolution2(char *s)
-{
-    int lastSize = 0;
-    int i = strlen(s) - 1;
-
-    if (s[i] == ' ')
-    {
-        while (i >= 0 && s[i] == ' ')
-        {
-            i--;
-        }
-    }
-
-    while (i >= 0)
-    {
-        if (s[i] == ' ')
-        {
-            break;
-        }
-        if ((s[i] >= 97 && s[i] <= 122) || (s[i] >= 65 && s[i] <= 90))
-        {
-            lastSize++;
-        }
+    while (i >= 0 && s[i] == ' ') {
         i--;
     }
 
-    return lastSize;
-}
-
-// Best Solution 2:
-int bestSolution2(char *s)
-{
-    int len = 0;
-    int i = strlen(s) - 1;
-
-    while (s[i] == ' ' && i >= 0)
-    {
+    while (i >= 0 && s[i] != ' ') {
+        length++;
         i--;
     }
-    while (i >= 0 && s[i--] != ' ')
-    {
-        len++;
-    }
 
-    return len;
+    return length;
 }
