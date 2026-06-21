c235306111eb2c326e2d83c1e25b7ce4a720a79a
hyungupark <hyungu.1215.park@gmail.com>
2026-06-22 07:05:34 +0900

    leetcode: "58_length_of_last_word" add javascript solution

--- leetcode/algorithms/58_length_of_last_word/main.js
+++ leetcode/algorithms/58_length_of_last_word/main.js
@@ -1,4 +1,12 @@
 /**
+ * Builtin function
+ *
+ * Complexities:
+ *   N - Length of `s`
+ *   - Time Complexity: O(N)
+ *   - Space Complexity: O(1)
+ */
+/**
  * @param {string} s
  * @return {number}
  */
@@ -8,14 +16,44 @@ var lengthOfLastWord = function (s) {
 };
 
 
-// Best Solution
-// Best Solution 1:
-var bestSolution1 = function (s) {
-  return s.trim().split(" ").pop().length;
+// Solution
+/**
+ * Solution 1
+ *
+ * Reverse Traversal
+ *
+ * Complexities:
+ *   N - Length of `s`
+ *   - Time Complexity: O(N)
+ *   - Space Complexity: O(1)
+ */
+var solution1 = function (s) {
+  let length = 0;
+  let i = s.length - 1;
+
+  while (i >= 0 && s[i] === " ") {
+    i--;
+  }
+
+  while (i >= 0 && s[i] !== " ") {
+    length++;
+    i--;
+  }
+
+  return length;
 };
 
-// Best Solution 2:
-var bestSolution2 = function (s) {
-  let trimmedString = s.trim();
-  return trimmedString.length - trimmedString.lastIndexOf(" ") - 1;
+/**
+ * Solution 2
+ *
+ * Built-in function
+ *
+ * Complexities:
+ *   N - Length of `s`
+ *   - Time Complexity: O(N)
+ *   - Space Complexity: O(N)
+ */
+var solution2 = function (s) {
+  const words = s.trim().split(" ");
+  return words[words.length - 1].length;
 };
