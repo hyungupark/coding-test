61540cf7da6fe1184bfb5e4f13c0d515d3ff488d
hyungupark <hyungu.1215.park@gmail.com>
2026-06-24 07:23:27 +0900

    leetcode: "66_plus_one" add c solution

--- leetcode/algorithms/66_plus_one/main.c
+++ leetcode/algorithms/66_plus_one/main.c
@@ -3,133 +3,70 @@
 /**
  * Note: The returned array must be malloced, assume caller calls free().
  */
-int *plusOne(int *digits, int digitsSize, int *returnSize)
-{
-    *returnSize = digitsSize;
-    int *result = (int *)malloc(sizeof(int) * *returnSize);
+
+/**
+ * Complexities:
+ *   N - digitsSize 
+ *   - Time Complexity: O(N)
+ *   - Space Complexity: O(N)
+ */
+int* plusOne(int* digits, int digitsSize, int* returnSize) {
+    int* result = (int*)malloc(sizeof(int) * (digitsSize + 1));
+
     int up = 1;
+    for (int i = digitsSize - 1; i >= 0; i--) {
+        digits[i] += up;
+        up = 0;
 
-    for (int i = digitsSize - 1; i >= 0; i--)
-    {
-        int num = digits[i] + up;
-        if (num >= 10)
-        {
-            num -= 10;
+        if (digits[i] > 9) {
+            result[i + 1] = digits[i] - 10;
             up = 1;
+        } else {
+            result[i + 1] = digits[i];
         }
-        else
-        {
-            up = 0;
-        }
-
-        result[i] = num;
     }
 
-    if (up > 0)
-    {
-        (*returnSize)++;
-        result = realloc(result, sizeof(int) * *returnSize);
-        result[0] = 1;
-        result[*returnSize - 1] = 0;
-    }
-
-    return result;
-}
-
-
-// Best Solution
-// Best Solution 1:
-int *bestSolution1(int *digits, int digitsSize, int *returnSize)
-{
     *returnSize = digitsSize;
-    int *plusOne = malloc(digitsSize * sizeof(int));
-    if (plusOne == NULL)
-    {
-        return (NULL);
-    }
-    for (int i = 0; i < digitsSize; i++)
-    {
-        plusOne[i] = digits[i];
-    }
-
-    plusOne[digitsSize - 1]++;
-    for (int i = digitsSize - 1; i - 1 >= 0; i--)
-    {
-        if (plusOne[i] == 10)
-        {
-            plusOne[i] = 0;
-            plusOne[i - 1]++;
-        }
-    }
-
-    if (plusOne[0] == 10)
-    {
+    if (up > 0) {
+        result[0] = 1;
         (*returnSize)++;
-        plusOne = realloc(plusOne, *returnSize * sizeof(int));
-        if (plusOne == NULL)
-        {
-            return (NULL);
-        }
-        memmove(plusOne + 1, plusOne, digitsSize * sizeof(int));
-        plusOne[0] = 1;
-        plusOne[1] = 0;
+        return result;
     }
-    return (plusOne);
+
+    return result + 1;
 }
 
-// Best Solution 2:
-int *bestSolution2(int *digits, int digitsSize, int *returnSize)
-{
-    for (int i = digitsSize - 1; i >= 0; i--)
-    {
-        digits[i]++;
-        if (digits[i] < 10)
-        {
+
+// Solution
+/**
+ * Complexities:
+ *   N - digitsSize 
+ *   - Time Complexity: O(N)
+ *   - Space Complexity: O(N)
+ */
+int* solution(int* digits, int digitsSize, int* returnSize) {
+    for (int i = digitsSize - 1; i >= 0; i--) {
+        if (digits[i] < 9) {
+            digits[i]++;
+
             *returnSize = digitsSize;
-            return digits;
+
+            int* result = (int*)malloc(sizeof(int) * digitsSize);
+            memcpy(result, digits, sizeof(int) * digitsSize);
+
+            return result;
         }
+
         digits[i] = 0;
     }
-    int *result = (int *)malloc((digitsSize + 1) * sizeof(int));
+
+    *returnSize = digitsSize + 1;
+    int* result = (int*)malloc(sizeof(int) * (*returnSize));
+
     result[0] = 1;
-    for (int i = 1; i <= digitsSize; i++)
-    {
+    for (int i = 1; i < *returnSize; i++) {
         result[i] = 0;
     }
-    *returnSize = digitsSize + 1;
+
     return result;
-}
-
-// Best Solution 3:
-int *bestSolution3(int *digits, int digitsSize, int *returnSize)
-{
-    int *res = malloc(sizeof(int) * 100);
-    int cf = 1;
-
-    for (int i = digitsSize - 1; i >= 0; i--)
-    {
-        if (digits[i] + cf > 9)
-        {
-            digits[i] = (digits[i] + cf) % 10;
-        }
-        else
-        {
-            digits[i] = digits[i] + cf;
-            cf = 0;
-            break;
-        }
-    }
-
-    if (cf)
-    {
-        res[0] = 1;
-    }
-
-    for (int i = cf, j = 0; j < digitsSize; i++, j++)
-    {
-        res[i] = digits[j];
-    }
-
-    *returnSize = digitsSize + cf;
-    return res;
 }
