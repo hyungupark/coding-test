132547bda0117970431b824900ee30ceafa4c816
hyungupark <hyungu.1215.park@gmail.com>
2026-06-25 01:06:12 +0900

    leetcode: "66_plus_one" add javascript solution

--- leetcode/algorithms/66_plus_one/main.js
+++ leetcode/algorithms/66_plus_one/main.js
@@ -1,67 +1,51 @@
 /**
+ * Complexities:
+ *   N - Length of `digits`
+ *   - Time Complexity: O(N)
+ *   - Space Complexity: O(N)
+ */
+/**
  * @param {number[]} digits
  * @return {number[]}
  */
 var plusOne = function (digits) {
-  digits.reverse();
-  let up = 0;
-  for (let i = 0; i < digits.length; i++) {
-    let result = digits[i] + up;
-    up = 0;
-    if (i === 0) {
-      result += 1;
-    }
-    if (result >= 10) {
-      up = 1;
-      digits[i] = result - 10;
-    } else {
-      digits[i] = result;
-    }
-  }
-  if (up > 0) {
-    digits.push(up);
-  }
-  return digits.reverse();
-};
-
-
-// Best Solution
-// Best Solution 1:
-var bestSolution1 = function (digits) {
-  for (var i = digits.length - 1; i >= 0; i--) {
-    digits[i]++;
-    if (digits[i] > 9) {
-      digits[i] = 0;
-    } else {
-      return digits;
-    }
-  }
-  digits.unshift(1);
-  return digits;
-};
-
-// Best Solution 2:
-var bestSolution2 = function (digits) {
-  for (var i = digits.length - 1; i >= 0; i--) {
-    if (++digits[i] > 9) {
-      digits[i] = 0;
-    } else {
-      return digits;
-    }
-  }
-  digits.unshift(1);
-  return digits;
-};
-
-// Best Solution 3:
-var bestSolution3 = function (digits) {
   for (let i = digits.length - 1; i >= 0; i--) {
-    if (digits[i] === 9) {
-      digits[i] = 0;
-    } else {
-      digits[i]++;
+    digits[i]++;
+
+    if (digits[i] < 10) {
       return digits;
     }
+
+    digits[i] -= 10;
   }
-  return [1, ...digits];
+
+  return new Array(1).fill(1).concat(new Array(digits.length).fill(0));
+};
+
+
+// Solution
+/**
+ * Complexities:
+ *   N - Length of `digits`
+ *   - Time Complexity: O(N)
+ *   - Space Complexity: O(1)
+ */
+/**
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+var solution = function (digits) {
+  for (let i = digits.length - 1; i >= 0; i--) {
+    digits[i]++;
+
+    if (digits[i] < 10) {
+      return digits;
+    }
+
+    digits[i] = 0;
+  }
+
+  digits.unshift(1);
+
+  return digits;
 };
