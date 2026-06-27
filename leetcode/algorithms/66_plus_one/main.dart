adfebd67aabaab2c93a8a553dab18cf78d380161
hyungupark <hyungu.1215.park@gmail.com>
2026-06-27 12:19:31 +0900

    leetcode: "66_plus_one" add dart solution

--- leetcode/algorithms/66_plus_one/main.dart
+++ leetcode/algorithms/66_plus_one/main.dart
@@ -1,36 +1,46 @@
 class PlusOne {
+  /**
+   * Complexities:
+   *   N - Length of `digits`
+   *   - Time Complexity: O(N)
+   *   - Space Complexity: O(N)
+   */
   List<int> plusOne(List<int> digits) {
-    int up = 1;
-
     for (int i = digits.length - 1; i >= 0; i--) {
-      int num = digits[i] + up;
-      if (num >= 10) {
-        num -= 10;
-        up = 1;
-      } else {
-        up = 0;
+      digits[i]++;
+
+      if (digits[i] < 10) {
+        return digits;
       }
 
-      digits[i] = num;
+      digits[i] -= 10;
     }
 
-    if (up > 0) {
-      digits.insert(0, 1);
-    }
-
-    return digits;
+    return [1] + List.filled(digits.length, 0);
   }
 
-  // Best Solution
-  List<int> bestSolution(List<int> digits) {
-    for (int i = digits.length - 1; i >= 0; i--) {
+
+  // Solution
+  /**
+   * Complexities:
+   *   N - Length of `digits`
+   *   - Time Complexity: O(N)
+   *   - Space Complexity: O(1)
+   */
+  List<int> solution(List<int> digits) {
+    int n = digits.length;
+
+    for (int i = n - 1; i >= 0; i--) {
       if (digits[i] < 9) {
         digits[i]++;
         return digits;
       }
+
       digits[i] = 0;
     }
+
     digits.insert(0, 1);
+
     return digits;
   }
 }
