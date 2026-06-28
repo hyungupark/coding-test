1d622397509356e43a6294b783466a88bba1baa6
hyungupark <hyungu.1215.park@gmail.com>
2026-06-29 01:02:45 +0900

    leetcode: "67_add_binary" add java solution

--- leetcode/algorithms/67_add_binary/main.java
+++ leetcode/algorithms/67_add_binary/main.java
@@ -1,53 +1,73 @@
 import java.math.BigInteger;
 
 class AddBinary {
+  /**
+   * BigInteger
+   *
+   * Complexities:
+   *   N - Length of `a`
+   *   M - Length of `b`
+   *   - Time Complexity: O(max(N, M))
+   *   - Space Complexity: O(max(N, M))
+   */
   public String addBinary(String a, String b) {
     return (new BigInteger(a, 2).add(new BigInteger(b, 2))).toString(2);
   }
 
 
-  // Best Solution
-  // Best Solution 1:
-  public String bestSolution1(String a, String b) {
+  // Solution
+  /**
+   * Solution 1
+   * 
+   * Reverse
+   *
+   * Complexities:
+   *   N - Length of `a`
+   *   M - Length of `b`
+   *   - Time Complexity: O(max(N, M))
+   *   - Space Complexity: O(max(N, M))
+   */
+  public String solution1(String a, String b) {
     StringBuilder sb = new StringBuilder();
-    int i = a.length() - 1, j = b.length() - 1, carry = 0;
-    while (i >= 0 || j >= 0) {
+
+    int i = a.length() - 1;
+    int j = b.length() - 1;
+
+    int carry = 0;
+
+    while (i >= 0 || j >= 0 || carry > 0) {
       int sum = carry;
+
       if (i >= 0) {
-        sum += a.charAt(i--) - '0';
+        sum += a.charAt(i) - '0';
+        i--;
       }
+
       if (j >= 0) {
-        sum += b.charAt(j--) - '0';
+        sum += b.charAt(j) - '0';
+        j--;
       }
+
       sb.append(sum % 2);
+
       carry = sum / 2;
     }
-    if (carry != 0) {
-      sb.append(carry);
-    }
+
     return sb.reverse().toString();
   }
 
-  // Best Solution 2:
-  public String bestSolution2(String a, String b) {
-    StringBuilder res = new StringBuilder();
-    int i = a.length() - 1;
-    int j = b.length() - 1;
-    int carry = 0;
-    while (i >= 0 || j >= 0) {
-      int sum = carry;
-      if (i >= 0) {
-        sum += a.charAt(i--) - '0';
-      }
-      if (j >= 0) {
-        sum += b.charAt(j--) - '0';
-      }
-      carry = sum > 1 ? 1 : 0;
-      res.append(sum % 2);
-    }
-    if (carry != 0) {
-      res.append(carry);
-    }
-    return res.reverse().toString();
+  /**
+   * Solution 2
+   * 
+   * BigInteger
+   *
+   * Complexities:
+   *   N - Length of `a`
+   *   M - Length of `b`
+   *   - Time Complexity: O(max(N, M))
+   *   - Space Complexity: O(max(N, M))
+   */
+  public String solution2(String a, String b) {
+    return new BigInteger(a, 2).add(new BigInteger(b, 2)).toString(2);
   }
 }
