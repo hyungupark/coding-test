a8eea72dfba63ffa8acf436e04fc969a4ffdcf52
hyungupark <hyungu.1215.park@gmail.com>
2026-07-05 13:58:25 +0900

    leetcode: "70_climbing_stairs" add python solution

--- leetcode/algorithms/70_climbing_stairs/main.py
+++ leetcode/algorithms/70_climbing_stairs/main.py
@@ -1,79 +1,89 @@
+from typing import List
+
+
 class ClimbingStairs:
+    """
+    # DP (Dynamic Programming) with Array
+    #
+    # Complexities:
+    #   N - Value of `n`
+    #   - Time Complexity: O(N)
+    #   - Space Complexity: O(N)
+    """
     def climbStairs(self, n: int) -> int:
-        if n < 2:
-            return 1
+        if n < 3:
+            return n
 
-        # Fibonacci numbers
-        dp = []
-        dp.append(1)
-        dp.append(2)
+        dp: List[int] = [0] * (n + 1)
+        dp[0] = 0
+        dp[1] = 1
+        dp[2] = 2
 
-        for i in range(0, n):
-            dp.append(dp[i - 1] + dp[i - 2])
+        for i in range(3, n + 1):
+            dp[i] = dp[i - 1] + dp[i - 2]
 
-        return dp[n - 1]
+        return dp[n]
 
 
-    # Best Solution
-    # Best Solution 1:
-    def bestSolution1(self, n: int) -> int:
-        if n == 0 or n == 1:
-            return 1
-        return self.climbStairs(n - 1) + self.climbStairs(n - 2)
+    # Solution
+    """
+    # Solution 1
+    #
+    # Recursion
+    #
+    # Complexities:
+    #   N - Value of `n`
+    #   - Time Complexity: O(2ᴺ)
+    #   - Space Complexity: O(1)
+    """
+    def solution1(self, n: int) -> int:
+        if n <= 2:
+            return n
 
-    # Best Solution 2:
-    # pure recursive (Can't pass the test case :does not work for big number, result time-exced limit)
-    #   - The base case will be when only 1 or 2 steps left
-    #   - Result time-exced limit
-    def bestSolution2(self, n: int) -> int:
-        def climb(n):
-            if n == 1:  # only one step option is availble
-                return 1
-            if (
-                n == 2
-            ):  # two options are possible : to take two 1-stpes or to only take one 2-steps
-                return 2
-            return climb(n - 1) + climb(n - 2)
+        return self.solution1(n-1) + self.solution1(n-2)
 
-        return climb(n)
+    """
+    # Solution 2
+    #
+    # DP (Dynamic Programming)
+    #
+    # Complexities:
+    #   N - Value of `n`
+    #   - Time Complexity: O(N)
+    #   - Space Complexity: O(1)
+    """
+    def solution2(self, n: int) -> int:
+        if n <= 2:
+            return n
 
-    # Best Solution 3:
-    # use dictionary (look-up table) to memorize repeating recursion
-    #   - The memory start with the base case and recored every recurssion
-    def bestSolution3(self, n: int) -> int:
-        memo = {}
-        memo[1] = 1
-        memo[2] = 2
+        prev2, prev1 = 1, 2
 
-        def climb(n):
-            if (
-                n in memo
-            ):  # if the recurssion already done before first take a look-up in the look-up table
-                return memo[n]
-            else:  # Store the recurssion function in the look-up table and reuturn the stored look-up table function
-                memo[n] = climb(n - 1) + climb(n - 2)
-                return memo[n]
+        for _ in range(3, n + 1):
+            current = prev1 + prev2
 
-        return climb(n)
+            prev2 = prev1
+            prev1 = current
 
-    # Best Solution 4:
-    # Dynamic programming
-    #   - store the distinct ways in a dynamic table
-    def bestSolution4(self, n: int) -> int:
-        def climb(n):
-            # edge cases
-            if n == 0:
-                return 0
-            if n == 1:
-                return 1
-            if n == 2:
-                return 2
-            dp = [0] * (n + 1)  # considering zero steps we need n+1 places
-            dp[1] = 1
-            dp[2] = 2
-            for i in range(3, n + 1):
-                dp[i] = dp[i - 1] + dp[i - 2]
-            print(dp)
-            return dp[n]
+        return prev1
 
-        return climb(n)
+    """
+    # Solution 3
+    #
+    # DP (Dynamic Programming) with Array
+    #
+    # Complexities:
+    #   N - Value of `n`
+    #   - Time Complexity: O(N)
+    #   - Space Complexity: O(N)
+    """
+    def solution3(self, n: int) -> int:
+        if n <= 2:
+            return n
+
+        dp = [0] * (n + 1)
+        dp[1], dp[2] = 1, 2
+
+        for i in range(3, n + 1):
+            dp[i] = dp[i-1] + dp[i-2]
+
+        return dp[n]
