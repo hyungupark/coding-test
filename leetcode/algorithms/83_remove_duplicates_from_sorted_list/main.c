525505c086e21268cfdee2f7527bc348faf654d2
hyungupark <hyungu.1215.park@gmail.com>
2026-07-07 07:13:21 +0900

    leetcode: "83_remove_duplicates_from_sorted_list" add c solution

--- leetcode/algorithms/83_remove_duplicates_from_sorted_list/main.c
+++ leetcode/algorithms/83_remove_duplicates_from_sorted_list/main.c
@@ -1,23 +1,59 @@
-#include <stdio.h>
+#include <stddef.h>
 
-struct ListNode
-{
+struct ListNode {
     int val;
-    struct ListNode *next;
+    struct ListNode* next;
 };
 
-struct ListNode *deleteDuplicates(struct ListNode *head)
-{
-    struct ListNode *current = head;
+/**
+ * Iteration
+ * 
+ * Complexities:
+ *   N - Size of `head`
+ *   - Time Complexity: O(N)
+ *   - Space Complexity: O(1)
+ */
+struct ListNode* deleteDuplicates(struct ListNode* head) {
+    struct ListNode* curr_head = head;
 
-    while (current)
-    {
-        if (current->next && current->next->val == current->val)
-        {
-            current->next = current->next->next;
+    while (curr_head) {
+        if (curr_head->next && curr_head->val == curr_head->next->val) {
+            curr_head->next = curr_head->next->next;
+        } else {
+            curr_head = curr_head->next;
         }
-        else
-        {
+    }
+
+    return head;
+}
+
+
+// Solution
+/**
+ * Solution 1
+ *
+ * Iteration
+ *
+ * Complexities:
+ *   N - Size of `head`
+ *   - Time Complexity: O(N)
+ *   - Space Complexity: O(1)
+ */
+struct ListNode* solution1(struct ListNode* head) {
+    if (head == NULL || head->next == NULL) {
+        return head;
+    }
+
+    struct ListNode* current = head;
+
+    while (current != NULL && current->next != NULL) {
+        if (current->val == current->next->val) {
+            struct ListNode* temp = current->next;
+            
+            current->next = current->next->next;
+            
+            free(temp);
+        } else {
             current = current->next;
         }
     }
@@ -25,69 +61,22 @@ struct ListNode *deleteDuplicates(struct ListNode *head)
     return head;
 }
 
-
-// Best Solution
-// Best Solution 1:
-struct ListNode *bestSolution1(struct ListNode *head)
-{
-    struct ListNode *temp = head;
-    while (temp && temp->next)
-    {
-        if (temp->next->val == temp->val)
-        {
-            temp->next = temp->next->next;
-            continue;
-        }
-        temp = temp->next;
-    }
-    return head;
-}
-
-// Best Solution 2:
-struct ListNode *bestSolution2(struct ListNode *head)
-{
-    struct ListNode *cur = head;
-    while (cur)
-    {
-        while (cur->next && cur->val == cur->next->val)
-        {
-            cur->next = cur->next->next;
-        }
-        cur = cur->next;
-    }
-    return head;
-}
-
-// Best Solution 3:
-struct ListNode *bestSolution3(struct ListNode *head)
-{
-    // Special case...
-    if (head == NULL || head->next == NULL)
-    {
+/**
+ * Solution 2
+ *
+ * Recursion
+ *
+ * Complexities:
+ *   N - Size of `head`
+ *   - Time Complexity: O(N)
+ *   - Space Complexity: O(N)
+ */
+struct ListNode* solution2(struct ListNode* head) {
+    if (head == NULL || head->next == NULL) {
         return head;
     }
-    // Initialize two pointers tmp(slow) and curr(fast)...
-    struct ListNode *tmp = head;
-    struct ListNode *curr = head->next;
-    // Traverse all element through a while loop if curr node is not null...
-    while (curr != NULL)
-    {
-        // If the value of curr is equal to the value of tmp...
-        // It means the value is present in the linked list...
-        if (tmp->val == curr->val)
-        {
-            // Hence we do not need to include curr again in the linked list...
-            // So we increment the value of curr...
-            curr = curr->next;
-        }
-        // Otherwise, we increment both the pointers.
-        else
-        {
-            tmp->next = curr;
-            tmp = curr;
-            curr = tmp->next;
-        }
-    }
-    tmp->next = NULL;
-    return head; // Return the sorted linked list without any duplicate element...
+
+    head->next = solution2(head->next);
+
+    return head->val == head->next->val ? head->next : head;
 }
