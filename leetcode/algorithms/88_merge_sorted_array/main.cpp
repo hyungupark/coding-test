8303f2149b7c6d22f090f07a6fd511f38fb5dc3a
hyungupark <hyungu.1215.park@gmail.com>
2026-07-10 01:21:21 +0900

    leetcode: "88_merge_sorted_array" add c++ solution

--- leetcode/algorithms/88_merge_sorted_array/main.cpp
+++ leetcode/algorithms/88_merge_sorted_array/main.cpp
@@ -1,69 +1,65 @@
 #include <vector>
 using namespace std;
 
-class MergeSortedArray
-{
+class MergeSortedArray {
 public:
-    void merge(vector<int> &nums1, int m, vector<int> &nums2, int n)
-    {
-        for (int i = 0; i < n; i++)
-        {
-            nums1[m + i] = nums2[i];
-        }
-        sort(nums1.begin(), nums1.end());
-    }
+    /**
+     * Three Pointers from the End
+     * 
+     * Complexities:
+     *   M - Size of `nums1`
+     *   N - Size of `nums2`
+     *   - Time Complexity: O(M + N)
+     *   - Time Complexity: O(1)
+     */
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int p1 = m - 1;
+        int p2 = n - 1;
+        int p = m + n - 1;
 
-
-    // Best Solution
-    // Best Solution 1: STL
-    void bestSolution1(vector<int> &nums1, int m, vector<int> &nums2, int n)
-    {
-        for (int j = 0, i = m; j < n; j++)
-        {
-            nums1[i] = nums2[j];
-            i++;
-        }
-        sort(nums1.begin(), nums1.end());
-    }
-
-    // Best Solution 2: Two Pointer
-    void bestSolution2(vector<int> &nums1, int m, vector<int> &nums2, int n)
-    {
-        int i = m - 1;
-        int j = n - 1;
-        int k = m + n - 1;
-
-        while (j >= 0)
-        {
-            if (i >= 0 && nums1[i] > nums2[j])
-            {
-                nums1[k--] = nums1[i--];
+        while (p2 >= 0) {
+            if (p1 >= 0 && nums1[p1] > nums2[p2]) {
+                nums1[p] = nums1[p1];
+                p1--;
+            } else {
+                nums1[p] = nums2[p2];
+                p2--;
             }
-            else
-            {
-                nums1[k--] = nums2[j--];
-            }
+            p--;
         }
     }
 
-    // Best Solution 3:
-    void bestSolution3(vector<int> &nums1, int m, vector<int> &nums2, int n)
-    {
-        int i = m - 1, j = n - 1, k = m + n - 1;
-        while (i >= 0 and j >= 0)
-        {
-            if (nums1[i] < nums2[j])
-            {
-                nums1[k--] = nums2[j--];
+
+    // Solution
+    /**
+     * Three Pointers from the End
+     *
+     * Complexities:
+     *   M - Size of `nums1`
+     *   N - Size of `nums2`
+     *   - Time Complexity: O(M + N)
+     *   - Time Complexity: O(1)
+     */
+    void solution(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int p1 = m - 1;
+        int p2 = n - 1;
+        int p = m + n - 1;
+
+        while (p1 >= 0 && p2 >= 0) {
+            if (nums1[p1] > nums2[p2]) {
+                nums1[p] = nums1[p1];
+                p1--;
+            } else {
+                nums1[p] = nums2[p2];
+                p2--;
             }
-            else
-            {
-                nums1[k--] = nums1[i--];
-            }
+            p--;
         }
-        while (j >= 0)
-        {
-            nums1[k--] = nums2[j--];
+
+        while (p2 >= 0) {
+            nums1[p] = nums2[p2];
+            p2--;
+            p--;
         }
     }
 };
