1b27bafe3c5510ae7c57695af28f45eb9ebb1172
hyungupark <hyungu.1215.park@gmail.com>
2026-07-11 18:39:00 +0900

    leetcode: "88_merge_sorted_array" add java solution

--- leetcode/algorithms/88_merge_sorted_array/main.java
+++ leetcode/algorithms/88_merge_sorted_array/main.java
@@ -1,28 +1,66 @@
-import java.util.Arrays;
-
 class MergeSortedArray {
+  /**
+   * Three Pointers from the End
+   *
+   * Complexities:
+   *   M - Size of `nums1`
+   *   N - Size of `nums2`
+   *   - Time Complexity: O(M + N)
+   *   - Time Complexity: O(1)
+   */
   public void merge(int[] nums1, int m, int[] nums2, int n) {
-    for (int i = 0; i < nums1.length; i++) {
-      if (i >= m) {
-        nums1[i] = nums2[i - m];
+    int p1 = m - 1;
+    int p2 = n - 1;
+    int p = m + n - 1;
+
+    while (p2 >= 0) {
+      if (p1 >= 0 && nums1[p1] > nums2[p2]) {
+        nums1[p] = nums1[p1];
+        p1--;
+      } else {
+        nums1[p] = nums2[p2];
+        p2--;
       }
+      p--;
     }
-    Arrays.sort(nums1);
   }
 
 
-  // Best Solution
-  // Best Solution 1: STL
-  public void bestSolution1(int[] nums1, int m, int[] nums2, int n) {
-    for (int j = 0, i = m; j < n; j++) {
-      nums1[i] = nums2[j];
-      i++;
+  // Solution
+  /**
+   * Three Pointers from the End
+   *
+   * Complexities:
+   *   M - Size of `nums1`
+   *   N - Size of `nums2`
+   *   - Time Complexity: O(M + N)
+   *   - Time Complexity: O(1)
+   */
+  public void solution(int[] nums1, int m, int[] nums2, int n) {
+    int p1 = m - 1;
+    int p2 = n - 1;
+    int p = m + n - 1;
+
+    while (p1 >= 0 && p2 >= 0) {
+      if (nums1[p1] > nums2[p2]) {
+        nums1[p] = nums1[p1];
+        p1--;
+      } else {
+        nums1[p] = nums2[p2];
+        p2--;
+      }
+      p--;
     }
-    Arrays.sort(nums1);
+
+    while (p2 >= 0) {
+      nums1[p] = nums2[p2];
+      p2--;
+      p--;
+    }
   }
 
-  // Best Solution 2: Two Pointer
-  public void bestSolution2(int[] nums1, int m, int[] nums2, int n) {
+  // Solution 2: Two Pointer
+  public void solution2(int[] nums1, int m, int[] nums2, int n) {
     int i = m - 1;
     int j = n - 1;
     int k = m + n - 1;
@@ -36,20 +74,22 @@ class MergeSortedArray {
     }
   }
 
-  // Best Solution 3:
-  public void bestSolution3(int[] nums1, int m, int[] nums2, int n) {
+  // Solution 3
+  public void solution3(int[] nums1, int m, int[] nums2, int n) {
     int tail1 = m - 1, tail2 = n - 1, finished = m + n - 1;
+
     while (tail1 >= 0 && tail2 >= 0) {
       nums1[finished--] = (nums1[tail1] > nums2[tail2]) ? nums1[tail1--] : nums2[tail2--];
     }
 
-    while (tail2 >= 0) { // only need to combine with remaining nums2, if any
+    // only need to combine with remaining nums2, if any
+    while (tail2 >= 0) {
       nums1[finished--] = nums2[tail2--];
     }
   }
 
-  // Best Solution 4:
-  public void bestSolution4(int[] nums1, int m, int[] nums2, int n) {
+  // Solution 4
+  public void solution4(int[] nums1, int m, int[] nums2, int n) {
     // variables to work as pointers
     int i = m - 1; // will point at m-1 index of nums1 array
     int j = n - 1; // will point at n-1 index of nums2 array
