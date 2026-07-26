8bb3e95ff066dd96ff2119df211f77d1c5bfd712
hyungupark <hyungu.1215.park@gmail.com>
2026-07-26 16:32:11 +0900

    leetcode: "108_convert_sorted_array_to_binary_search_tree" add c++ solution

--- leetcode/algorithms/108_convert_sorted_array_to_binary_search_tree/main.cpp
+++ leetcode/algorithms/108_convert_sorted_array_to_binary_search_tree/main.cpp
@@ -1,38 +1,40 @@
 #include <vector>
 using namespace std;
 
-class ConvertSortedArrayToBinarySearchTree
-{
+class ConvertSortedArrayToBinarySearchTree {
 public:
-    struct TreeNode
-    {
+    struct TreeNode {
         int val;
-        TreeNode *left;
-        TreeNode *right;
+        TreeNode* left;
+        TreeNode* right;
         TreeNode() : val(0), left(nullptr), right(nullptr) {}
         TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-        TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+        TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
     };
 
-    TreeNode *sortedArrayToBST(vector<int> &nums)
-    {
-        if (nums.size() == 0)
-        {
+    /**
+     * Divide-and-conquer
+     *
+     * Complexities:
+     *   N - Size of `nums`
+     *   - Time Complexity: O(N)
+     *   - Space Complexity: O(logᴺ)
+     */
+    TreeNode* sortedArrayToBST(vector<int>& nums) {
+        if (nums.size() == 0) {
             return nullptr;
         }
 
         return convertToBST(nums, 0, nums.size() - 1);
     }
 
-    TreeNode *convertToBST(vector<int> nums, int left, int right)
-    {
-        if (right < left)
-        {
+    TreeNode* convertToBST(vector<int> nums, int left, int right) {
+        if (right < left) {
             return nullptr;
         }
 
         int mid = (left + right) / 2;
-        TreeNode *node = new TreeNode(nums[mid]);
+        TreeNode* node = new TreeNode(nums[mid]);
         node->left = convertToBST(nums, left, mid - 1);
         node->right = convertToBST(nums, mid + 1, right);
 
@@ -40,47 +42,36 @@ public:
     }
 
 
-    // Best Solution
-    // Best Solution 1: Recursion
-    TreeNode *bestSolution1(vector<int> &nums)
-    {
-        if (nums.size() == 0)
-        {
-            return NULL;
+    // Solution
+    /**
+     * Divide-and-conquer
+     *
+     * Complexities:
+     *   N - Size of `nums`
+     *   - Time Complexity: O(N)
+     *   - Space Complexity: O(logᴺ)
+     */
+    TreeNode* buildBST(const std::vector<int>& nums, int left, int right) {
+        if (left > right) {
+            return nullptr;
         }
-        if (nums.size() == 1)
-        {
-            return new TreeNode(nums[0]);
-        }
 
-        int middle = nums.size() / 2;
-        TreeNode *root = new TreeNode(nums[middle]);
+        int mid = left + (right - left) / 2;
 
-        vector<int> leftInts(nums.begin(), nums.begin() + middle);
-        vector<int> rightInts(nums.begin() + middle + 1, nums.end());
+        TreeNode* node = new TreeNode(nums[mid]);
 
-        root->left = bestSolution1(leftInts);
-        root->right = bestSolution1(rightInts);
+        node->left = buildBST(nums, left, mid - 1);
+        
+        node->right = buildBST(nums, mid + 1, right);
 
-        return root;
+        return node;
     }
 
-    // Best Solution 2: Recursion
-    TreeNode *bestSolution2(vector<int> &nums, int start, int end)
-    {
-        if (end <= start)
-        {
-            return NULL;
+    TreeNode* solution(std::vector<int>& nums) {
+        if (nums.empty()) {
+            return nullptr;
         }
-        int midIdx = (end + start) / 2;
-        TreeNode *root = new TreeNode(nums[midIdx]);
-        root->left = bestSolution2(nums, start, midIdx);
-        root->right = bestSolution2(nums, midIdx + 1, end);
-        return root;
-    }
 
-    TreeNode *bestSolution2(vector<int> &nums)
-    {
-        return bestSolution2(nums, 0, nums.size());
+        return buildBST(nums, 0, nums.size() - 1);
     }
 };
