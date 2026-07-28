52ec326186db9aa7e528c90a48c8000cabf953f5
hyungupark <hyungu.1215.park@gmail.com>
2026-07-29 01:10:44 +0900

    leetcode: "110_balanced_binary_tree" add c++ solution

--- leetcode/algorithms/110_balanced_binary_tree/main.cpp
+++ leetcode/algorithms/110_balanced_binary_tree/main.cpp
@@ -1,87 +1,87 @@
-#include <stdlib.h>
 #include <algorithm>
+#include <cstdlib>
 using namespace std;
 
-class BalancedBinaryTree
-{
-    struct TreeNode
-    {
+class BalancedBinaryTree {
+    struct TreeNode {
         int val;
-        TreeNode *left;
-        TreeNode *right;
+        TreeNode* left;
+        TreeNode* right;
         TreeNode() : val(0), left(nullptr), right(nullptr) {}
         TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-        TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+        TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
     };
 
 public:
-    bool isBalanced(TreeNode *root)
-    {
-        if (root)
-        {
-            int depth = getDepth(root->left) - getDepth(root->right);
-            if (depth < 0)
-            {
-                depth *= -1;
-            }
-            if (depth > 1)
-            {
-                return false;
-            }
-            if (!isBalanced(root->left) || !isBalanced(root->right))
-            {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    int getDepth(struct TreeNode *root)
-    {
-        if (root)
-        {
-            int left = getDepth(root->left);
-            int right = getDepth(root->right);
-            return (left > right ? left : right) + 1;
-        }
-        return 0;
-    }
-
-
-    // Best Solution
-    bool bestSolution(TreeNode *root)
-    {
-        // If the tree is empty, we can say it’s balanced...
-        if (root == NULL)
-        {
-            return true;
-        }
-        // Height Function will return -1, when it’s an unbalanced tree...
-        if (Height(root) == -1)
-        {
-            return false;
-        }
-        return true;
-    }
-
-    // Create a function to return the “height” of a current subtree using recursion...
-    int Height(TreeNode *root)
-    {
-        // Base case...
-        if (root == NULL)
-        {
+    /**
+     * DFS
+     *
+     * Complexities:
+     *   N - Number of `node`
+     *   H - Height of `node`
+     *   - Time Complexity: O(N)
+     *   - Space Complexity: O(H)
+     */
+    int getMaxDepth(TreeNode* node) {
+        if (node == nullptr) {
             return 0;
         }
-        // Height of left subtree...
-        int leftHeight = Height(root->left);
-        // Height of height subtree...
-        int rightHight = Height(root->right);
-        // In case of left subtree or right subtree unbalanced or their heights differ by more than ‘1’, return -1...
-        if (leftHeight == -1 || rightHight == -1 || abs(leftHeight - rightHight) > 1)
-        {
+
+        int left_depth = getMaxDepth(node->left);
+        if (left_depth < 0) {
             return -1;
         }
-        // Otherwise, return the height of this subtree as max(leftHeight, rightHight) + 1...
-        return max(leftHeight, rightHight) + 1;
+
+        int right_depth = getMaxDepth(node->right);
+        if (right_depth < 0) {
+            return -1;
+        }
+
+        if (abs(left_depth - right_depth) > 1) {
+            return -1;
+        }
+
+        return max(left_depth, right_depth) + 1;
+    }
+
+    bool isBalanced(TreeNode* root) {
+        return getMaxDepth(root) != -1;
+    }
+
+
+    // Solution
+    /**
+     * DFS
+     *
+     * Complexities:
+     *   N - Number of `node`
+     *   H - Height of `node`
+     *   - Time Complexity: O(N)
+     *   - Space Complexity: O(H)
+     */
+    int checkHeight(TreeNode* node) {
+        if (node == nullptr) {
+            return 0;
+        }
+
+        int leftHeight = checkHeight(node->left);
+        if (leftHeight == -1) {
+            return -1;
+        }
+
+        int rightHeight = checkHeight(node->right);
+        if (rightHeight == -1) {
+            return -1;
+        }
+
+        if (std::abs(leftHeight - rightHeight) > 1) {
+            return -1;
+        }
+
+        return 1 + std::max(leftHeight, rightHeight);
+    }
+
+    bool solution(TreeNode* root) {
+        return checkHeight(root) != -1;
     }
 };
