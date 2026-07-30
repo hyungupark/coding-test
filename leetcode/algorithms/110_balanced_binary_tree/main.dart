487fac9cd775ffef86840df178636fe4e0142288
hyungupark <hyungu.1215.park@gmail.com>
2026-07-31 02:02:53 +0900

    leetcode: "110_balanced_binary_tree" add dart solution

--- leetcode/algorithms/110_balanced_binary_tree/main.dart
+++ leetcode/algorithms/110_balanced_binary_tree/main.dart
@@ -8,37 +8,75 @@ class TreeNode {
 }
 
 class BalancedBinaryTree {
+  /**
+   * DFS
+   *
+   * Complexities:
+   *   N - Number of `node`
+   *   H - Height of `node`
+   *   - Time Complexity: O(N)
+   *   - Space Complexity: O(H)
+   */
+  int _getMaxDepth(TreeNode? node) {
+    if (node == null) {
+      return 0;
+    }
+
+    int leftDepth = _getMaxDepth(node.left);
+    if (leftDepth < 0) {
+      return -1;
+    }
+
+    int rightDepth = _getMaxDepth(node.right);
+    if (rightDepth < 0) {
+      return -1;
+    }
+
+    if ((leftDepth - rightDepth).abs() > 1) {
+      return -1;
+    }
+
+    return max(leftDepth, rightDepth) + 1;
+  }
+
   bool isBalanced(TreeNode? root) {
-    if (root == null) {
-      return true;
-    }
-
-    int returnDepth(TreeNode? node) {
-      if (node == null) {
-        return 0;
-      }
-      return max(returnDepth(node.left), returnDepth(node.right)) + 1;
-    }
-
-    if ((returnDepth(root.left) - returnDepth(root.right)).abs() > 1) {
-      return false;
-    }
-
-    return isBalanced(root.left) && isBalanced(root.right);
+    return _getMaxDepth(root) != -1;
   }
 
-  // Best Solution
-  bool bestSolution(TreeNode? root) {
-    return cnt(root) != -1;
+
+  // Solution
+  /**
+   * DFS
+   *
+   * Complexities:
+   *   N - Number of `node`
+   *   H - Height of `node`
+   *   - Time Complexity: O(N)
+   *   - Space Complexity: O(H)
+   */
+  bool solution(TreeNode? root) {
+    return _checkHeightAndBalance(root) != -1;
   }
 
-  int cnt(TreeNode? nod) {
-    if (nod == null) return 0;
-    int l = cnt(nod.left);
-    if (l == -1) return -1;
-    int r = cnt(nod.right);
-    if (r == -1) return -1;
-    if ((r - l > 1) || (l - r > 1)) return -1;
-    return (l >= r ? l + 1 : r + 1);
+  int _checkHeightAndBalance(TreeNode? node) {
+    if (node == null) {
+      return 0;
+    }
+
+    int leftHeight = _checkHeightAndBalance(node.left);
+    if (leftHeight == -1) {
+      return -1;
+    }
+
+    int rightHeight = _checkHeightAndBalance(node.right);
+    if (rightHeight == -1) {
+      return -1;
+    }
+
+    if ((leftHeight - rightHeight).abs() > 1) {
+      return -1;
+    }
+
+    return 1 + max(leftHeight, rightHeight);
   }
 }
