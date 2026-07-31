882e9e15999aa4794a1da0826df30768bb6ad816
hyungupark <hyungu.1215.park@gmail.com>
2026-08-01 02:05:33 +0900

    leetcode: "111_minimum_depth_of_binary_tree" add c solution

--- leetcode/algorithms/111_minimum_depth_of_binary_tree/main.c
+++ leetcode/algorithms/111_minimum_depth_of_binary_tree/main.c
@@ -1,76 +1,66 @@
 #include <stdlib.h>
 
-struct TreeNode
-{
+struct TreeNode {
     int val;
-    struct TreeNode *left;
-    struct TreeNode *right;
+    struct TreeNode* left;
+    struct TreeNode* right;
 };
 
-int minDepth(struct TreeNode *root)
-{
-    if (!root)
-    {
+/**
+ * Recursion: DFS
+ * 
+ * Complexities:
+ *   N - Number of nodes in `root`
+ *   H - Height of `root`
+ *   - Time Complexity: O(N)
+ *   - Space Complexity: O(H)
+ */
+int minDepth(struct TreeNode* root) {
+    if (!root) {
         return 0;
     }
 
-    int left = minDepth(root->left);
-    int right = minDepth(root->right);
+    int left_depth = minDepth(root->left);
+    int right_depth = minDepth(root->right);
 
-    if (!left || !right)
-    {
-        return (left > right ? left : right) + 1;
+    if (!left_depth || !right_depth) {
+        return (left_depth > right_depth ? left_depth : right_depth) + 1;
     }
 
-    return (left > right ? right : left) + 1;
+    return (left_depth < right_depth ? left_depth : right_depth) + 1;
 }
 
 
-// Best Solution
-// Best Solution 1:
-int bestSolution1(struct TreeNode *root)
-{
-    if (root == NULL)
-    {
-        return 0;
-    }
-    else
-    {
-        int leftDepth = minDepth(root->left);
-        int rightDepth = minDepth(root->right);
-        if (leftDepth > rightDepth)
-        {
-            return rightDepth + 1;
-        }
-        else
-        {
-            return leftDepth + 1;
-        }
-    }
+// Solution
+/**
+ * Recursion: DFS
+ * 
+ * Complexities:
+ *   N - Number of nodes in `root`
+ *   H - Height of `root`
+ *   - Time Complexity: O(N)
+ *   - Space Complexity: O(H)
+ */
+ // 두 수 중 작은 값을 구하는 헬퍼 함수
+int min(int a, int b) {
+    return (a < b) ? a : b;
 }
 
-// Best Solution 2: Recursive
-int bestSolution2(struct TreeNode *root)
-{
-    if (!root)
-    {
+int minDepth(struct TreeNode* root) {
+    if (root == NULL) {
         return 0;
     }
 
-    if (!root->left && !root->right)
-    {
+    if (root->left == NULL && root->right == NULL) {
         return 1;
     }
 
-    if (!root->left)
-    {
-        return minDepth(root->right) + 1;
+    if (root->left == NULL) {
+        return 1 + minDepth(root->right);
+    }
+    if (root->right == NULL) {
+        return 1 + minDepth(root->left);
     }
 
-    if (!root->right)
-    {
-        return minDepth(root->left) + 1;
-    }
-
-    return minDepth(root->left) > minDepth(root->right) ? minDepth(root->right) + 1 : minDepth(root->left) + 1;
+    return 1 + min(minDepth(root->left), minDepth(root->right));
 }
