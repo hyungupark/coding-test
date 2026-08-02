b71ed069bff27d22ea81d51c8c3714f10a3a5800
hyungupark <hyungu.1215.park@gmail.com>
2026-08-03 01:10:41 +0900

    leetcode: "111_minimum_depth_of_binary_tree" add dart solution

--- leetcode/algorithms/111_minimum_depth_of_binary_tree/main.dart
+++ leetcode/algorithms/111_minimum_depth_of_binary_tree/main.dart
@@ -1,3 +1,4 @@
+import 'dart:collection';
 import 'dart:math';
 
 class TreeNode {
@@ -7,7 +8,16 @@ class TreeNode {
   TreeNode([this.val = 0, this.left, this.right]);
 }
 
-class Solution {
+class MinimumDepthOfBinaryTree {
+  /**
+   * Recursion: DFS
+   * 
+   * Complexities:
+   *   N - Number of nodes in `root`
+   *   H - Height of `root`
+   *   - Time Complexity: O(N)
+   *   - Space Complexity: O(H)
+   */
   int minDepth(TreeNode? root) {
     if (root == null) {
       return 0;
@@ -16,23 +26,82 @@ class Solution {
     int leftDepth = minDepth(root.left);
     int rightDepth = minDepth(root.right);
 
-    if (leftDepth == 0 && rightDepth == 0) {
-      return 1;
-    } else if (leftDepth == 0) {
-      return rightDepth + 1;
-    } else if (rightDepth == 0) {
-      return leftDepth + 1;
+    if (leftDepth == 0 || rightDepth == 0) {
+      return max(leftDepth, rightDepth) + 1;
     }
 
     return min(leftDepth, rightDepth) + 1;
   }
 
-  // Best Solution
-  int bestSolution(TreeNode? root) {
-    if (root == null) return 0;
-    int l = minDepth(root.left);
-    int r = minDepth(root.right);
-    if (l == 0 || r == 0) return l + r + 1;
-    return min(l, r) + 1;
+
+  // Solution
+  /**
+   * Solution 1
+   * 
+   * Iteration: BFS (Queue)
+   *
+   * Complexities:
+   *   N - Number of nodes in `root`
+   *   W - Width of `root`
+   *   - Time Complexity: O(N)
+   *   - Space Complexity: O(W)
+   */
+  int solution1(TreeNode? root) {
+    if (root == null) {
+      return 0;
+    }
+
+    Queue<TreeNode> queue = Queue();
+    queue.add(root);
+    int depth = 1;
+
+    while (queue.isNotEmpty) {
+      int levelSize = queue.length;
+
+      for (int i = 0; i < levelSize; i++) {
+        TreeNode current = queue.removeFirst();
+
+        if (current.left == null && current.right == null) {
+          return depth;
+        }
+
+        if (current.left != null) {
+          queue.add(current.left!);
+        }
+        if (current.right != null) {
+          queue.add(current.right!);
+        }
+      }
+
+      depth++;
+    }
+
+    return depth;
+  }
+
+  /**
+   * Solution 2
+   * 
+   * Recursion: DFS
+   * 
+   * Complexities:
+   *   N - Number of nodes in `root`
+   *   H - Height of `root`
+   *   - Time Complexity: O(N)
+   *   - Space Complexity: O(H)
+   */
+  int solution2(TreeNode? root) {
+    if (root == null) {
+      return 0;
+    }
+
+    if (root.left == null) {
+      return solution2(root.right) + 1;
+    }
+    if (root.right == null) {
+      return solution2(root.left) + 1;
+    }
+
+    return min(solution2(root.left), solution2(root.right)) + 1;
   }
 }
