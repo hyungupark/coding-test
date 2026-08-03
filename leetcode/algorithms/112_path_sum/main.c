6a29de435783ea03e04a950083c6cb886756e925
hyungupark <hyungu.1215.park@gmail.com>
2026-08-04 01:10:57 +0900

    leetcode: "112_path_sum" add c solution

--- leetcode/algorithms/112_path_sum/main.c
+++ leetcode/algorithms/112_path_sum/main.c
@@ -1,34 +1,37 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
-struct TreeNode
-{
+struct TreeNode {
     int val;
-    struct TreeNode *left;
-    struct TreeNode *right;
+    struct TreeNode* left;
+    struct TreeNode* right;
 };
 
-bool hasPathSum(struct TreeNode *root, int targetSum)
-{
-    if (!root)
-    {
+/**
+ * Recursion - DFS
+ * 
+ * Complexities:
+ *   N - Number of nodes in `root`
+ *   H - Height of `root`
+ *   - Time Complexity: O(N)
+ *   - Space Complexity: O(N)
+ */
+bool hasPathSum(struct TreeNode* root, int targetSum) {
+    if (!root) {
         return false;
     }
 
     targetSum -= root->val;
 
-    if (!root->left && !root->right)
-    {
+    if (!root->left && !root->right) {
         return targetSum == 0;
     }
 
-    if (!root->left)
-    {
+    if (!root->left) {
         return hasPathSum(root->right, targetSum);
     }
 
-    if (!root->right)
-    {
+    if (!root->right) {
         return hasPathSum(root->left, targetSum);
     }
 
@@ -36,38 +39,26 @@ bool hasPathSum(struct TreeNode *root, int targetSum)
 }
 
 
-// Best Solution
-// Best Solution 1:
-bool bestSolution1(struct TreeNode *root, int targetSum)
-{
-    // If the tree is empty i.e. root is NULL, return false...
-    if (root == NULL)
-    {
+// Solution
+/**
+ * Recursion - DFS
+ * 
+ * Complexities:
+ *   N - Number of nodes in `root`
+ *   H - Height of `root`
+ *   - Time Complexity: O(N)
+ *   - Space Complexity: O(N)
+ */
+bool solution(struct TreeNode* root, int targetSum) {
+    if (root == NULL) {
         return false;
     }
 
-    // If there is only a single root node and the value of root node is equal to the targetSum...
-    if (root->val == targetSum && (root->left == NULL && root->right == NULL))
-    {
-        return true;
+    if (root->left == NULL && root->right == NULL) {
+        return (targetSum == root->val);
     }
 
-    // Call the same function recursively for left and right subtree...
-    return hasPathSum(root->left, targetSum - root->val) || hasPathSum(root->right, targetSum - root->val);
-}
+    int remainingSum = targetSum - root->val;
 
-// Best Solution 2:
-bool bestSolution2(struct TreeNode *root, int targetSum)
-{
-    if (!root)
-    {
-        return false;
-    }
-
-    if (!root->right && !root->left)
-    {
-        return targetSum == root->val;
-    }
-
-    return hasPathSum(root->left, targetSum - root->val) || hasPathSum(root->right, targetSum - root->val);
+    return solution(root->left, remainingSum) || solution(root->right, remainingSum);
 }
