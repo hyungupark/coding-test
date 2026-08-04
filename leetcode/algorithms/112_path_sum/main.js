90bbb35c30ced7c6e7b0d9d161de0302b5d9552d
hyungupark <hyungu.1215.park@gmail.com>
2026-08-05 01:34:30 +0900

    leetcode: "112_path_sum" add javascript solution

--- leetcode/algorithms/112_path_sum/main.js
+++ leetcode/algorithms/112_path_sum/main.js
@@ -5,6 +5,41 @@ function TreeNode(val, left, right) {
 }
 
 /**
+ * Recursion - DFS
+ *
+ * Complexities:
+ *   N - Number of nodes in `root`
+ *   H - Height of `root`
+ *   - Time Complexity: O(N)
+ *   - Space Complexity: O(N)
+ */
+/**
+ * @param {number} targetSum_
+ * @param {number} sum_
+ * @param {TreeNode} node_
+ * @return {boolean}
+ */
+const checkSum = (targetSum, sum, depth, node) => {
+  if (!node) {
+    return false;
+  }
+
+  if (node.val === targetSum && !node.left && !node.right && depth === 1) {
+    return true;
+  }
+
+  const sum = sum + node.val;
+  if (sum === targetSum && depth > 1 && !node.left && !node.right) {
+    return true;
+  } else {
+    return (
+      checkSum(targetSum, sum, depth + 1, node.left) ||
+      checkSum(targetSum, sum, depth + 1, node.right)
+    );
+  }
+};
+
+/**
  * @param {TreeNode} root
  * @param {number} targetSum
  * @return {boolean}
@@ -13,67 +48,67 @@ var hasPathSum = function (root, targetSum) {
   return checkSum(targetSum, 0, 1, root);
 };
 
+
+// Solution
 /**
- * @param {number} targetSum_
- * @param {number} sum_
- * @param {TreeNode} node_
- * @return {boolean}
+ * Solution 1
+ *
+ * Recursion - DFS
+ *
+ * Complexities:
+ *   N - Number of nodes in `root`
+ *   H - Height of `root`
+ *   - Time Complexity: O(N)
+ *   - Space Complexity: O(N)
  */
-const checkSum = (targetSum_, sum_, depth_, node_) => {
-  if (!node_) {
+var solution1 = function (root, targetSum) {
+  if (root === null) {
     return false;
   }
 
-  if (node_.val === targetSum_ && !node_.left && !node_.right && depth_ === 1) {
-    return true;
+  if (root.left === null && root.right === null) {
+    return targetSum === root.val;
   }
 
-  const sum = sum_ + node_.val;
-  if (sum === targetSum_ && depth_ > 1 && !node_.left && !node_.right) {
-    return true;
-  } else {
-    return (
-      checkSum(targetSum_, sum, depth_ + 1, node_.left) ||
-      checkSum(targetSum_, sum, depth_ + 1, node_.right)
-    );
-  }
-};
+  const remainingSum = targetSum - root.val;
 
-
-// Best Solution
-// Best Solution 1: Recursion
-var bestSolution1 = function (root, sum) {
-  if (!root) {
-    return false;
-  }
-
-  if (!root.left && !root.right) {
-    // check leaf
-    return sum === root.val;
-  } else {
-    // continue DFS
-    return (
-      hasPathSum(root.left, sum - root.val) ||
-      hasPathSum(root.right, sum - root.val)
-    );
-  }
-};
-
-// Best Solution 2:
-var bestSolution2 = function (root, sum) {
-  // If the tree is empty i.e. root is NULL, return false...
-  if (root == null) {
-    return false;
-  }
-
-  // If there is only a single root node and the value of root node is equal to the targetSum...
-  if (root.val == targetSum && root.left == null && root.right == null) {
-    return true;
-  }
-
-  // Call the same function recursively for left and right subtree...
   return (
-    hasPathSum(root.left, targetSum - root.val) ||
-    hasPathSum(root.right, targetSum - root.val)
+    solution1(root.left, remainingSum) || solution1(root.right, remainingSum)
   );
+};
+
+/**
+ * Solution 2
+ *
+ * Iteration - DFS with Stack
+ *
+ * Complexities:
+ *   N - Number of nodes in `root`
+ *   H - Height of `root`
+ *   - Time Complexity: O(N)
+ *   - Space Complexity: O(N)
+ */
+var solution2 = function (root, targetSum) {
+  if (root === null) {
+    return false;
+  }
+
+  const stack = [[root, targetSum - root.val]];
+
+  while (stack.length > 0) {
+    const [node, currentTarget] = stack.pop();
+
+    if (node.left === null && node.right === null && currentTarget === 0) {
+      return true;
+    }
+
+    if (node.right !== null) {
+      stack.push([node.right, currentTarget - node.right.val]);
+    }
+    if (node.left !== null) {
+      stack.push([node.left, currentTarget - node.left.val]);
+    }
+  }
+
+  return false;
 };
