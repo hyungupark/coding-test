7adf7cac1dd08a3edc5eb4c5719521b4da835d86
hyungupark <hyungu.1215.park@gmail.com>
2026-08-07 01:28:43 +0900

    leetcode: "118_pascals_triangle" add c++ solution

--- leetcode/algorithms/118_pascals_triangle/main.cpp
+++ leetcode/algorithms/118_pascals_triangle/main.cpp
@@ -1,24 +1,25 @@
 #include <vector>
 using namespace std;
 
-class PascalsTriangle
-{
+class PascalsTriangle {
 public:
-    vector<vector<int>> generate(int numRows)
-    {
+    /**
+     * DP
+     * 
+     * Complexities:
+     *   N - `numRows`
+     *   - Time Complexity: O(N²)
+     *   - Space Complexity: O(N²)
+     */
+    vector<vector<int>> generate(int numRows) {
         vector<vector<int>> result;
 
-        for (int i = 0; i < numRows; i++)
-        {
+        for (int i = 0; i < numRows; i++) {
             vector<int> row;
-            for (int j = 0; j <= i; j++)
-            {
-                if (j == 0 || j == i)
-                {
+            for (int j = 0; j <= i; j++) {
+                if (j == 0 || j == i) {
                     row.push_back(1);
-                }
-                else
-                {
+                } else {
                     row.push_back(result[i - 1][j - 1] + result[i - 1][j]);
                 }
             }
@@ -28,59 +29,25 @@ public:
         return result;
     }
 
-    // Best Solution 1: Using Recursion
-    vector<vector<int>> bestSolution1(int numRows)
-    {
-        if (numRows == 0)
-            return {};
-        if (numRows == 1)
-            return {{1}};
 
-        vector<vector<int>> prevRows = bestSolution1(numRows - 1);
-        vector<int> newRow(numRows, 1);
+    // Solution
+    /**
+     * DP
+     * 
+     * Complexities:
+     *   N - `numRows`
+     *   - Time Complexity: O(N²)
+     *   - Space Complexity: O(N²)
+     */
+    vector<vector<int>> solution(int numRows) {
+        vector<vector<int>> result(numRows);
 
-        for (int i = 1; i < numRows - 1; i++)
-        {
-            newRow[i] = prevRows.back()[i - 1] + prevRows.back()[i];
-        }
+        for (int i = 0; i < numRows; ++i) {
+            result[i].resize(i + 1, 1);
 
-        prevRows.push_back(newRow);
-        return prevRows;
-    }
-
-    // Best Solution 2: Using Combinatorial Formula
-    vector<vector<int>> bestSolution2(int numRows)
-    {
-        vector<vector<int>> result;
-        for (int i = 0; i < numRows; i++)
-        {
-            vector<int> row(i + 1, 1);
-            for (int j = 1; j < i; j++)
-            {
-                row[j] = result[i - 1][j - 1] + result[i - 1][j];
+            for (int j = 1; j < i; ++j) {
+                result[i][j] = result[i - 1][j - 1] + result[i - 1][j];
             }
-            result.push_back(row);
-        }
-        return result;
-    }
-
-    // Best Solution 3: Using Dynamic Programming with 1D Array
-    vector<vector<int>> bestSolution3(int numRows)
-    {
-        vector<vector<int>> result;
-        vector<int> prevRow;
-
-        for (int i = 0; i < numRows; i++)
-        {
-            vector<int> currentRow(i + 1, 1);
-
-            for (int j = 1; j < i; j++)
-            {
-                currentRow[j] = prevRow[j - 1] + prevRow[j];
-            }
-
-            result.push_back(currentRow);
-            prevRow = currentRow;
         }
 
         return result;
