470996eab9cbb5ee6f94b383fb62ff9aeee2bac1
hyungupark <hyungu.1215.park@gmail.com>
2026-08-17 12:22:11 +0900

    leetcode: "125_valid_palindrome" add java solution

--- leetcode/algorithms/125_valid_palindrome/main.java
+++ leetcode/algorithms/125_valid_palindrome/main.java
@@ -1,35 +1,73 @@
 class ValidPalindrome {
-
+  /**
+   * Regex
+   *
+   * Complexities:
+   *   N - Size of `s`
+   *   - Time Complexity: O(N)
+   *   - Space Complexity: O(1)
+   */
   public boolean isPalindrome(String s) {
     s = s.toLowerCase().replaceAll("[^a-z0-9]", "");
-    for (int i = 0, j = s.length() - 1; i < j; i++, j--) {
-      if (s.charAt(i) != s.charAt(j)) {
+
+    for (int left = 0, right = s.length() - 1; left < right; left++, right--) {
+      if (s.charAt(left) != s.charAt(right)) {
         return false;
       }
     }
+
     return true;
   }
 
-  // Best Solution
-  public boolean bestSolution(String s) {
-    if (s.isEmpty()) {
-      return true;
-    }
-    int head = 0, tail = s.length() - 1;
-    char cHead, cTail;
-    while (head <= tail) {
-      cHead = s.charAt(head);
-      cTail = s.charAt(tail);
-      if (!Character.isLetterOrDigit(cHead)) {
-        head++;
-      } else if (!Character.isLetterOrDigit(cTail)) {
-        tail--;
+
+  // Solution
+  /**
+   * Solution 1
+   * 
+   * Regex
+   *
+   * Complexities:
+   *   N - Size of `s`
+   *   - Time Complexity: O(N)
+   *   - Space Complexity: O(1)
+   */
+  public boolean solution1(String s) {
+    String cleaned = s.replaceAll("[^A-Za-z0-9]", "").toLowerCase();
+
+    String reversed = new StringBuilder(cleaned).reverse().toString();
+
+    return cleaned.equals(reversed);
+  }
+
+  /*
+   * Solution 2
+   * 
+   * Two-Pointers
+   * 
+   * Complexities:
+   *   N - Size of `s`
+   *   - Time Complexity: O(N)
+   *   - Space Complexity: O(1)
+   */
+  public boolean solution2(String s) {
+    int left = 0;
+    int right = s.length() - 1;
+
+    while (left < right) {
+      char charLeft = s.charAt(left);
+      char charRight = s.charAt(right);
+
+      if (!Character.isLetterOrDigit(charLeft)) {
+        left++;
+      } else if (!Character.isLetterOrDigit(charRight)) {
+        right--;
       } else {
-        if (Character.toLowerCase(cHead) != Character.toLowerCase(cTail)) {
+        if (Character.toLowerCase(charLeft) != Character.toLowerCase(charRight)) {
           return false;
         }
-        head++;
-        tail--;
+
+        left++;
+        right--;
       }
     }
 
