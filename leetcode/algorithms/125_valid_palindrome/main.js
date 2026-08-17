b5ea30c4afac9020d8a3ca2cead3933bb8daf86d
hyungupark <hyungu.1215.park@gmail.com>
2026-08-17 12:21:55 +0900

    leetcode: "125_valid_palindrome" add javascript solution

--- leetcode/algorithms/125_valid_palindrome/main.js
+++ leetcode/algorithms/125_valid_palindrome/main.js
@@ -1,37 +1,96 @@
 /**
+ * Regex
+ *
+ * Complexities:
+ *   N - Size of `s`
+ *   - Time Complexity: O(N)
+ *   - Space Complexity: O(1)
+ */
+/**
  * @param {string} s
  * @return {boolean}
  */
 var isPalindrome = function (s) {
-  s = s.toLowerCase().replace(/[^a-z0-9]/gi, '');
+  s = s.toLowerCase().replace(/[^a-z0-9]/gi, "");
   i = 0;
+
   while (i < s.length - 1 - i) {
     if (s[i] !== s[s.length - 1 - i]) {
       return false;
     }
     i++;
   }
+
   return true;
 };
 
-var isPalindrome = function (s) {
-  s = s.toLowerCase().replace(/[^a-z0-9]/gi, '');
-  for (let i = 0; i < s.length; i++) {
-    if (i === s.length - 1 - i) {
-      break;
+
+// Solution
+/**
+ * Solution 1
+ *
+ * Regex
+ *
+ * Complexities:
+ *   N - Size of `s`
+ *   - Time Complexity: O(N)
+ *   - Space Complexity: O(1)
+ */
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var solution1 = function (s) {
+  const cleaned = s.toLowerCase().replace(/[^a-z0-9]/g, "");
+
+  const reversed = cleaned.split("").reverse().join("");
+
+  return cleaned === reversed;
+};
+
+/**
+ * Solution 2
+ *
+ * Two-Pointers
+ *
+ * Complexities:
+ *   N - Size of `s`
+ *   - Time Complexity: O(N)
+ *   - Space Complexity: O(1)
+ */
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var solution2 = function (s) {
+  let left = 0;
+  let right = s.length - 1;
+
+  const isAlphanumeric = (char) => {
+    const code = char.charCodeAt(0);
+    return (
+      (code >= 48 && code <= 57) ||
+      (code >= 65 && code <= 90) ||
+      (code >= 97 && code <= 122)
+    );
+  };
+
+  while (left < right) {
+    while (left < right && !isAlphanumeric(s[left])) {
+      left++;
     }
-    if (s[i] !== s[s.length - 1 - i]) {
+
+    while (left < right && !isAlphanumeric(s[right])) {
+      right--;
+    }
+
+    if (s[left].toLowerCase() !== s[right].toLowerCase()) {
       return false;
     }
-  }
-  return true;
-};
 
-// Best Solution
-const bestSolution = (s) => {
-  s = s.toLowerCase().replace(/[^a-z0-9]/gi, '');
-  for (let i = 0, j = s.length - 1; i <= j; i++, j--) {
-    if (s.charAt(i) !== s.charAt(j)) return false;
+    left++;
+    right--;
   }
+
   return true;
 };
