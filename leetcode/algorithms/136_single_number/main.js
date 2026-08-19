9e4e2ad44f7419d896af0909f90c4df0fb769d8b
hyungupark <hyungu.1215.park@gmail.com>
2026-08-20 01:44:53 +0900

    leetcode: "136_single_number" add javascript solution

--- leetcode/algorithms/136_single_number/main.js
+++ leetcode/algorithms/136_single_number/main.js
@@ -1,28 +1,65 @@
 /**
+ * Bit Manipulation
+ *
+ * Complexities:
+ *   N - Size of `nums`
+ *   - Time Complexity: O(N)
+ *   - Space Complexity: O(1)
+ */
+/**
  * @param {number[]} nums
  * @return {number}
  */
 var singleNumber = function (nums) {
-  let index = 0;
-  while (index < nums.length) {
-    const findIndex = nums
-      .filter((v_, i_) => i_ !== index)
-      .findIndex((v_) => v_ === nums[index]);
-    if (findIndex === -1) {
-      return nums[index];
-    }
-    index++;
+  let result = 0;
+
+  for (const num of nums) {
+    result ^= num;
   }
+
+  return result;
 };
 
-// Best Solution
-var bestSolution = function (nums) {
-  // Initialize the unique number...
-  let uniqNum = 0;
-  // TRaverse all elements through the loop...
-  for (let idx = 0; idx < nums.length; idx++) {
-    // Concept of XOR...
-    uniqNum = uniqNum ^ nums[idx];
+
+// Solution
+/**
+ * Solution 1
+ *
+ * XOR
+ *
+ * Complexities:
+ *   N - Size of `nums`
+ *   - Time Complexity: O(N)
+ *   - Space Complexity: O(1)
+ */
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var solution1 = function (nums) {
+  let result = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    result ^= nums[i];
   }
-  return uniqNum; // Return the unique number...
+
+  return result;
+};
+
+/**
+ * Solution 2
+ *
+ * Array.prototype.reduce
+ *
+ * Complexities:
+ *   N - Size of `nums`
+ *   - Time Complexity: O(N)
+ *   - Space Complexity: O(1)
+ */
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var solution2 = function (nums) {
+  return nums.reduce((acc, current) => acc ^ current, 0);
 };
