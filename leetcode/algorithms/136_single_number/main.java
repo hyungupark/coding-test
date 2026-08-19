a0a0d7f5c14b4fd9180820dac22be329064e0792
hyungupark <hyungu.1215.park@gmail.com>
2026-08-20 01:45:17 +0900

    leetcode: "136_single_number" add java solution

--- leetcode/algorithms/136_single_number/main.java
+++ leetcode/algorithms/136_single_number/main.java
@@ -1,19 +1,39 @@
 class SingleNumber {
-
+  /**
+   * Bit Manipulation
+   *
+   * Complexities:
+   *   N - Size of `nums`
+   *   - Time Complexity: O(N)
+   *   - Space Complexity: O(1)
+   */
   public int singleNumber(int[] nums) {
     int result = 0;
+
     for (int num : nums) {
       result = result ^ num;
     }
+
     return result;
   }
 
-  // Best Solution
-  public int bestSolution(int[] nums) {
+
+  // Solution
+  /**
+   * XOR
+   *
+   * Complexities:
+   *   N - Size of `nums`
+   *   - Time Complexity: O(N)
+   *   - Space Complexity: O(1)
+   */
+  public int solution1(int[] nums) {
     int result = 0;
-    for (int i = 0; i < nums.length; i++) {
-      result = result ^ nums[i];
+
+    for (int num : nums) {
+      result = result ^ num;
     }
+
     return result;
   }
 }
