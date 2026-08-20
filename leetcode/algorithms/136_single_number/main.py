c158bc4934f2bd29ba5998f9bed3552754c8f7be
hyungupark <hyungu.1215.park@gmail.com>
2026-08-21 01:05:15 +0900

    leetcode: "136_single_number" add python solution

--- leetcode/algorithms/136_single_number/main.py
+++ leetcode/algorithms/136_single_number/main.py
@@ -1,14 +1,85 @@
+from collections import Counter
 from typing import List
+import functools
+import operator
 
 
-class Solution:
+class SingleNumber:
+    """
+    # Bit Manipulation
+    #
+    # Complexities:
+    #   N - Size of `nums`
+    #   - Time Complexity: O(N)
+    #   - Space Complexity: O(1)
+    """
     def singleNumber(self, nums: List[int]) -> int:
         result = 0
+
         for num in nums:
             result = result ^ num
+
         return result
 
 
-    # Best Solution
-    def bestSolution(self, nums: List[int]) -> int:
-        return reduce(lambda total, el: total ^ el, nums)
+    # Solution
+    """
+    # Solution 1
+    #
+    # XOR
+    #
+    # Complexities:
+    #   N - Size of `nums`
+    #   - Time Complexity: O(N)
+    #   - Space Complexity: O(1)
+    """
+    def solution1(self, nums: List[int]) -> int:
+        result = 0
+
+        for num in nums:
+            result ^= num
+
+        return result
+
+    """
+    # Solution 2
+    #
+    # functools.reduce + operator.xor
+    #
+    # Complexities:
+    #   N - Size of `nums`
+    #   - Time Complexity: O(N)
+    #   - Space Complexity: O(1)
+    """
+    def solution2(self, nums: List[int]) -> int:
+        return functools.reduce(operator.xor, nums)
+
+    """
+    # Solution 3
+    #
+    # HashMap
+    #
+    # Complexities:
+    #   N - Size of `nums`
+    #   - Time Complexity: O(N)
+    #   - Space Complexity: O(1)
+    """
+    def solution3(self, nums: List[int]) -> int:
+        counts = Counter(nums)
+
+        for num, count in counts.items():
+            if count == 1:
+                return num
+
+    """
+    # Solution 4
+    #
+    # Set
+    #
+    # Complexities:
+    #   N - Size of `nums`
+    #   - Time Complexity: O(N)
+    #   - Space Complexity: O(1)
+    """
+    def solution4(self, nums: List[int]) -> int:
+        return 2 * sum(set(nums)) - sum(nums)
