f64f760a493e9048e46144c464060714bf1b4cd0
hyungupark <hyungu.1215.park@gmail.com>
2026-08-21 01:05:30 +0900

    leetcode: "136_single_number" add dart solution

--- leetcode/algorithms/136_single_number/main.dart
+++ leetcode/algorithms/136_single_number/main.dart
@@ -1,18 +1,55 @@
 class SingleNumber {
+  /**
+   * Bit Manipulation
+   * 
+   * Complexities:
+   *   N - Size of `nums`
+   *   - Time Complexity: O(N)
+   *   - Space Complexity: O(1)
+   */
   int singleNumber(List<int> nums) {
     int result = 0;
-    for (int num in nums) {
+
+    for (final int num in nums) {
       result ^= num;
     }
+
     return result;
   }
 
-  // Best Solution
-  int bestSolution(List<int> nums) {
+
+  // Solution
+  /**
+   * Solution 1
+   * 
+   * XOR
+   * 
+   * Complexities:
+   *   N - Size of `nums`
+   *   - Time Complexity: O(N)
+   *   - Space Complexity: O(1)
+   */
+  int solution1(List<int> nums) {
     int result = 0;
+
     for (int num in nums) {
-      result = result ^ num;
+      result ^= num;
     }
+
     return result;
+  }
+
+  /**
+   * Solution 2
+   * 
+   * reduce()
+   * 
+   * Complexities:
+   *   N - Size of `nums`
+   *   - Time Complexity: O(N)
+   *   - Space Complexity: O(1)
+   */
+  int solution2(List<int> nums) {
+    return nums.reduce((a, b) => a ^ b);
   }
 }
