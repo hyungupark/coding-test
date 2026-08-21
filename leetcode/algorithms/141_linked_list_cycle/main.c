1c550fb5d320696d23b3ab6bab6972c032dc8922
hyungupark <hyungu.1215.park@gmail.com>
2026-08-22 01:35:53 +0900

    leetcode: "141_linked_list_cycle" add c solution

--- leetcode/algorithms/141_linked_list_cycle/main.c
+++ leetcode/algorithms/141_linked_list_cycle/main.c
@@ -1,22 +1,27 @@
 #include <stdbool.h>
+#include <stdlib.h>
 
-struct ListNode
-{
+struct ListNode {
     int val;
-    struct ListNode *next;
+    struct ListNode* next;
 };
 
-bool hasCycle(struct ListNode *head)
-{
-    struct ListNode *node1 = head;
-    struct ListNode *node2 = head;
+/**
+ * Floyd's Tortoise and Hare Algorithm (Two Pointers)
+ * 
+ * Complexities:
+ *   N - Number of nodes in `head`
+ *   - Time Complexity: O(N)
+ *   - Space Complexity: O(1)
+ */
+bool hasCycle(struct ListNode* head) {
+    struct ListNode* ahead = head;
+    struct ListNode* behind = head;
 
-    while (node2 != 0 && node2->next != 0)
-    {
-        node1 = node1->next;
-        node2 = node2->next->next;
-        if (node1 == node2)
-        {
+    while (ahead && ahead->next) {
+        ahead = ahead->next->next;
+        behind = behind->next;
+        if (ahead == behind) {
             return true;
         }
     }
@@ -24,20 +29,32 @@ bool hasCycle(struct ListNode *head)
     return false;
 }
 
-// Best Solution
-bool bestSolution(struct ListNode *head)
-{
-    // Initialize two node slow and fast point to the hesd node...
-    struct ListNode *fastptr = head;
-    struct ListNode *slowptr = head;
-    while (fastptr != NULL && fastptr->next != NULL)
-    {
-        // Move slow pointer by 1 node and fast at 2 at each step.
-        slowptr = slowptr->next;
-        fastptr = fastptr->next->next;
-        // If both the pointers meet at any point, then the cycle is present and return true...
-        if (slowptr == fastptr)
-            return true;
+
+// Solution
+/**
+ * Floyd's Tortoise and Hare Algorithm (Two Pointers)
+ * 
+ * Complexities:
+ *   N - Number of nodes in `head`
+ *   - Time Complexity: O(N)
+ *   - Space Complexity: O(1)
+ */
+bool solution(struct ListNode* head) {
+    if (head == NULL || head->next == NULL) {
+        return false;
     }
+
+    struct ListNode *slow = head;
+    struct ListNode *fast = head;
+
+    while (fast != NULL && fast->next != NULL) {
+        slow = slow->next;
+        fast = fast->next->next;
+
+        if (slow == fast) {
+            return true;
+        }
+    }
+
     return false;
 }
