5cc922e3d4ca1fdb580b93910937a422a39d61f5
hyungupark <hyungu.1215.park@gmail.com>
2026-08-24 01:07:32 +0900

    leetcode: "141_linked_list_cycle" add python solution

--- leetcode/algorithms/141_linked_list_cycle/main.py
+++ leetcode/algorithms/141_linked_list_cycle/main.py
@@ -1,11 +1,5 @@
 from typing import Optional
 
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
-
 
 class ListNode:
     def __init__(self, x):
@@ -13,38 +7,74 @@ class ListNode:
         self.next = None
 
 
-class Solution:
+class LinkedListCycle:
+    """
+    # Floyd's Tortoise and Hare Algorithm (Two Pointers)
+    #
+    # Complexities:
+    #   N - Number of nodes in `head`
+    #   - Time Complexity: O(N)
+    #   - Space Complexity: O(1)
+    """
     def hasCycle(self, head: Optional[ListNode]) -> bool:
-        stack = []
-        while head != None:
-            if head in stack:
+        ahead: Optional[ListNode] = head
+        behind: Optional[ListNode] = head
+
+        while ahead != None and ahead.next != None:
+            ahead = ahead.next.next
+            behind = behind.next
+
+            if ahead == behind:
                 return True
-            stack.append(head)
-            head = head.next
+
         return False
 
 
-    # Best Solution
-    # Dictionary/Hash table
-    def bestSolution1(self, head: ListNode) -> bool:
-        dictionary = {}
-        while head:
-            if head in dictionary:
+    # Solution
+    """
+    # Solution 1
+    #
+    # HashSet
+    #
+    # Complexities:
+    #   N - Number of nodes in `head`
+    #   - Time Complexity: O(N)
+    #   - Space Complexity: O(N)
+    """
+    def solution1(self, head: ListNode) -> bool:
+        visited = set()
+        curr = head
+        
+        while curr:
+            if curr in visited:
                 return True
-            else:
-                dictionary[head] = True
-            head = head.next
+            visited.add(curr)
+            curr = curr.next
+            
         return False
 
-    # Two pointers
-    def bestSolution2(self, head: ListNode) -> bool:
-        if not head:
+    """
+    # Solution 2
+    #
+    # Floyd's Tortoise and Hare Algorithm (Two Pointers)
+    #
+    # Complexities:
+    #   N - Number of nodes in `head`
+    #   - Time Complexity: O(N)
+    #   - Space Complexity: O(1)
+    """
+    def solution2(self, head: ListNode) -> bool:
+        if not head or not head.next:
             return False
+            
         slow = head
-        fast = head.next
-        while slow != fast:
-            if not fast or not fast.next:
-                return False
+        fast = head
+        
+        while fast and fast.next:
             slow = slow.next
             fast = fast.next.next
-        return True
+            
+            if slow == fast:
+                return True
+                
+        return False
