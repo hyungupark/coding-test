a32abd41572307a246c329f5072b7ba517c4cd3e
hyungupark <hyungu.1215.park@gmail.com>
2026-08-24 01:07:58 +0900

    leetcode: "144_binary_tree_preorder_traversal" add c solution

--- leetcode/algorithms/144_binary_tree_preorder_traversal/main.c
+++ leetcode/algorithms/144_binary_tree_preorder_traversal/main.c
@@ -1,16 +1,22 @@
 #include <stdlib.h>
 
-struct TreeNode
-{
+struct TreeNode {
     int val;
-    struct TreeNode *left;
-    struct TreeNode *right;
+    struct TreeNode* left;
+    struct TreeNode* right;
 };
 
-void addVal(struct TreeNode *node, int *returnSize, int *result)
-{
-    if (node != 0)
-    {
+/**
+ * Recursion
+ * 
+ * Complexities:
+ *   N - Number of nodes in `root`
+ *   H - Height of `root`
+ *   - Time Complexity: O(N)
+ *   - Space Complexity: O(H)
+ */
+void addVal(struct TreeNode* node, int* returnSize, int* result) {
+    if (node != 0) {
         result[*returnSize] = node->val;
         *returnSize = (*returnSize) + 1;
         addVal(node->left, returnSize, result);
@@ -18,31 +24,85 @@ void addVal(struct TreeNode *node, int *returnSize, int *result)
     }
 }
 
-int *preorderTraversal(struct TreeNode *root, int *returnSize)
-{
-    int *result = (int *)malloc(sizeof(int) * 100);
+int* preorderTraversal(struct TreeNode* root, int* returnSize) {
+    int* result = (int*)malloc(sizeof(int) * 100);
     *returnSize = 0;
     addVal(root, returnSize, result);
     return result;
 }
 
-// Best Solution
-void preorder(struct TreeNode *root, int *arr, int *returnSize)
-{
-    if (root)
-    {
-        arr[(*returnSize)++] = root->val;
-        preorder(root->left, arr, returnSize);
-        preorder(root->right, arr, returnSize);
+
+// Solution
+/**
+ * Solution 1
+ * 
+ * Recursion
+ * 
+ * Complexities:
+ *   N - Number of nodes in `root`
+ *   H - Height of `root`
+ *   - Time Complexity: O(N)
+ *   - Space Complexity: O(H)
+ */
+void preorder(struct TreeNode* node, int* result, int* idx) {
+    if (node == NULL) {
+        return;
     }
-    return;
+
+    result[*idx] = node->val;
+    (*idx)++;
+
+    preorder(node->left, result, idx);
+
+    preorder(node->right, result, idx);
 }
 
-int *bestSolution(struct TreeNode *root, int *returnSize)
-{
-    int *arr = (int *)malloc(sizeof(int) * 100);
+int* solution1(struct TreeNode* root, int* returnSize) {
+    int* result = (int*)malloc(sizeof(int) * 100);
     *returnSize = 0;
-    preorder(root, arr, returnSize);
-    return arr;
-    free(arr);
+
+    preorder(root, result, returnSize);
+
+    return result;
+}
+
+/**
+ * Solution 2
+ * 
+ * Iteration
+ * 
+ * Complexities:
+ *   N - Number of nodes in `root`
+ *   H - Height of `root`
+ *   - Time Complexity: O(N)
+ *   - Space Complexity: O(H)
+ */
+int* solution2(struct TreeNode* root, int* returnSize) {
+    int* result = (int*)malloc(sizeof(int) * 100);
+    *returnSize = 0;
+
+    if (root == NULL) {
+        return result;
+    }
+
+    struct TreeNode* stack[100];
+    int top = -1;
+
+    stack[++top] = root;
+
+    while (top >= 0) {
+        struct TreeNode* curr = stack[top--];
+        
+        result[(*returnSize)++] = curr->val;
+
+        if (curr->right != NULL) {
+            stack[++top] = curr->right;
+        }
+
+        if (curr->left != NULL) {
+            stack[++top] = curr->left;
+        }
+    }
+
+    return result;
 }
