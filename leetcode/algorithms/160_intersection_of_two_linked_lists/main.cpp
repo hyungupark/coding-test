4d7bdac7c889aaf6268173d9471a06484179f368
hyungupark <hyungu.1215.park@gmail.com>
2026-08-31 01:31:01 +0900

    leetcode: "160_intersection_of_two_linked_lists" add c++ solution

--- leetcode/algorithms/160_intersection_of_two_linked_lists/main.cpp
+++ leetcode/algorithms/160_intersection_of_two_linked_lists/main.cpp
@@ -1,58 +1,63 @@
-struct ListNode
-{
+#include<cstddef>
+using namespace std;
+
+struct ListNode {
     int val;
-    ListNode *next;
+    ListNode* next;
     ListNode(int x) : val(x), next(NULL) {}
 };
 
-class IntersectionOfTwoLinkedLists
-{
+class IntersectionOfTwoLinkedLists {
 public:
-    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB)
-    {
-        while (headA != nullptr)
-        {
-            ListNode *temp = headB;
-            while (temp != nullptr)
-            {
-                if (temp == headA)
-                    return temp;
-                temp = temp->next;
-            }
-            headA = headA->next;
+    /**
+     * Two-Pointer
+     *
+     * Complexities:
+     *   N - Number of Nodes in `headA`
+     *   M - Number of Nodes in `headB`
+     *   - Time Complexity: O(M + N)
+     *   - Space Complexity: O(1)
+     */
+    ListNode* getIntersectionNode(ListNode* headA, ListNode* headB) {
+        if (headA == nullptr || headB == nullptr) {
+            return nullptr;
         }
-        return nullptr;
+
+        ListNode* ptrA = headA;
+        ListNode* ptrB = headB;
+
+        while (ptrA != ptrB) {
+            ptrA = (ptrA == nullptr) ? headB : ptrA->next;
+            ptrB = (ptrB == nullptr) ? headA : ptrB->next;
+        }
+
+        return ptrA;
     }
 
-    // Best Solution
-    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB)
-    {
-        struct ListNode *p1 = headA;
-        struct ListNode *p2 = headB;
 
-        if (p1 == nullptr || p2 == nullptr)
+    // Solution
+    /**
+     * Two-Pointer
+     *
+     * Complexities:
+     *   N - Number of Nodes in `headA`
+     *   M - Number of Nodes in `headB`
+     *   - Time Complexity: O(M + N)
+     *   - Space Complexity: O(1)
+     */
+    ListNode* solution(ListNode* headA, ListNode* headB) {
+        if (headA == nullptr || headB == nullptr) {
             return nullptr;
-
-        while (p1 != nullptr && p2 != nullptr && p1 != p2)
-        {
-            p1 = p1->next;
-            p2 = p2->next;
-
-            // Any time they collide or reach end together without colliding
-            // then return any one of the pointers.
-            if (p1 == p2)
-                return p1;
-
-            // If one of them reaches the end earlier then reuse it
-            // by moving it to the beginning of other list.
-            // Once both of them go through reassigning,
-            // they will be equidistant from the collision point.
-            if (p1 == nullptr)
-                p1 = headB;
-            if (p2 == nullptr)
-                p2 = headA;
         }
 
-        return p1;
+        ListNode* pA = headA;
+        ListNode* pB = headB;
+
+        while (pA != pB) {
+            pA = (pA == nullptr) ? headB : pA->next;
+            pB = (pB == nullptr) ? headA : pB->next;
+        }
+
+        return pA;
     }
 };
