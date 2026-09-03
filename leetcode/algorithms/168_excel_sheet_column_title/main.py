8e1761a859a16a69c7ab55d35f6fe1cf27acf540
hyungupark <hyungu.1215.park@gmail.com>
2026-09-04 01:25:24 +0900

    leetcode: "168_excel_sheet_column_title" add python solution

--- leetcode/algorithms/168_excel_sheet_column_title/main.py
+++ leetcode/algorithms/168_excel_sheet_column_title/main.py
@@ -1,4 +1,10 @@
-class Solution:
+class ExcelSheetColumnTitle:
+    """
+    # Complexities:
+    #   N - `columnNumber`
+    #   - Time Complexity: O(logᴺ)
+    #   - Space Complexity: O(logᴺ)
+    """
     def convertToTitle(self, columnNumber: int) -> str:
         alphabet = [
             "A",
@@ -38,18 +44,23 @@ class Solution:
         return result
 
 
-    # Best Solution
-    def bestSolution1(self, columnNumber):
-        # Create an empty string for storing the characters...
-        output = ""
-        # Run a while loop while columnNumber is positive...
+    # Solution
+    """
+    # Complexities:
+    #   N - `columnNumber`
+    #   - Time Complexity: O(logᴺ)
+    #   - Space Complexity: O(logᴺ)
+    """
+    def solution(self, columnNumber):
+        result = []
+        
         while columnNumber > 0:
-            # Subtract 1 from columnNumber and get current character by doing modulo of columnNumber by 26...
-            output = chr(ord("A") + (columnNumber - 1) % 26) + output
-            # Divide columnNumber by 26...
-            columnNumber = (columnNumber - 1) // 26
-        # Return the output string.
-        return output
-
-    def bestSolution2(self, columnNumber):
-        return "" if columnNumber == 0 else self.convertToTitle((columnNumber - 1) / 26) + chr((columnNumber - 1) % 26 + ord("A"))
+            columnNumber -= 1
+            
+            remainder = columnNumber % 26
+            
+            result.append(chr(ord('A') + remainder))
+            
+            columnNumber //= 26
+            
+        return "".join(result[::-1])
