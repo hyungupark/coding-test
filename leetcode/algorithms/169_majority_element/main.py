6c3f9ee3c42af1cca8df1357d860469cb434f7a1
hyungupark <hyungu.1215.park@gmail.com>
2026-09-07 01:08:14 +0900

    leetcode: "169_majority_element" add python solution

--- leetcode/algorithms/169_majority_element/main.py
+++ leetcode/algorithms/169_majority_element/main.py
@@ -1,8 +1,16 @@
-from sys import stdin
+from collections import Counter, defaultdict
 from typing import List
 
 
-class Solution:
+class MajorityElement:
+    """
+    # HashMap
+    # 
+    # Complexities:
+    #   N - `numsSize`
+    #   - Time Complexity: O(N)
+    #   - Space Complexity: O(N)
+    """
     def majorityElement(self, nums: List[int]) -> int:
         numsMap = {}
         length = len(nums) / 2
@@ -18,11 +26,56 @@ class Solution:
                 return num
 
 
-# Best Solution
-import statistics
+    # Solution
+    """
+    # Solution 1
+    #
+    # HashMap
+    # 
+    # Complexities:
+    #   N - `numsSize`
+    #   - Time Complexity: O(N)
+    #   - Space Complexity: O(N)
+    """
+    def solution1(self, nums: List[int]) -> int:
+        counts = Counter(nums)
+        return max(counts.keys(), key=counts.get)
 
-f = open("user.out", "w")
-for line in stdin:
-    l = sorted(map(int, line.rstrip()[1:-1].split(",")))
-    print(l[len(l) // 2], file=f)
-exit(0)
+    """
+    # Solution 2
+    #
+    # Sorting
+    # 
+    # Complexities:
+    #   N - `numsSize`
+    #   - Time Complexity: O(N * logᴺ)
+    #   - Space Complexity: O(1)
+    """
+    def solution2(self, nums: List[int]) -> int:
+        nums.sort()
+        return nums[len(nums) // 2]
+
+    """
+    # Solution 3
+    #
+    # Boyer-Moore Voting Algorithm
+    #
+    # Complexities:
+    #   N - `numsSize`
+    #   - Time Complexity: O(N)
+    #   - Space Complexity: O(1)
+    """
+    def solution3(self, nums: List[int]) -> int:
+        candidate = None
+        count = 0
+        
+        for num in nums:
+            if count == 0:
+                candidate = num
+            
+            if num == candidate:
+                count += 1
+            else:
+                count -= 1
+                
+        return candidate
