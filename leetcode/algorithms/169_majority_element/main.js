b4b682ad8034993b911e7ea28d7ca111a42918a1
hyungupark <hyungu.1215.park@gmail.com>
2026-09-06 16:53:11 +0900

    leetcode: "169_majority_element" add javascript solution

--- leetcode/algorithms/169_majority_element/main.js
+++ leetcode/algorithms/169_majority_element/main.js
@@ -1,39 +1,59 @@
 /**
+ * HashMap
+ * 
+ * Complexities:
+ *   N - `numsSize`
+ *   - Time Complexity: O(N)
+ *   - Space Complexity: O(N)
+ */
+/**
  * @param {number[]} nums
  * @return {number}
  */
 var majorityElement = function (nums) {
   const numsMap = {};
   const length = nums.length / 2;
+
   while (nums) {
     const num = nums.pop();
+
     numsMap[num] = numsMap[num] ? numsMap[num] + 1 : 1;
+
     if (numsMap[num] >= length) {
       return num;
     }
   }
 };
 
-// Best Solution
-var bestSolution = function (nums) {
-  // Initialize sol and cnt to store the solution and its frequency for respective iterations...
-  let sol = 0, cnt = 0;
-  // For every element i in the array...
+
+// Solution
+/**
+ * Boyer-Moore Voting Algorithm
+ *
+ * Complexities:
+ *   N - `numsSize`
+ *   - Time Complexity: O(N)
+ *   - Space Complexity: O(1)
+ */
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var solution = function (nums) {
+  let candidate = null;
+  let count = 0;
+
   for (let i = 0; i < nums.length; i++) {
-    // If cnt is equal to zero, update sol as sol = i
-    if (cnt == 0) {
-      sol = nums[i];
-      cnt = 1;
+    if (count === 0) {
+      candidate = nums[i];
     }
-    // If i is equal to candidate, increment cnt...
-    else if (sol == nums[i]) {
-      cnt++;
-    }
-    // Otherwise, decrement cnt...
-    else {
-      cnt--;
+
+    if (nums[i] === candidate) {
+      count++;
+    } else {
+      count--;
     }
   }
-  // Return & print the solution...
-  return sol;
+
+  return candidate;
 };
