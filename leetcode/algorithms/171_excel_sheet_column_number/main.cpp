aa2e1f22027e67d23572e68e4f7cae9ccde0db43
hyungupark <hyungu.1215.park@gmail.com>
2026-09-08 01:48:21 +0900

    leetcode: "171_excel_sheet_column_number" add c++ solution

--- leetcode/algorithms/171_excel_sheet_column_number/main.cpp
+++ leetcode/algorithms/171_excel_sheet_column_number/main.cpp
@@ -1,29 +1,41 @@
 #include <string>
 using namespace std;
 
-class ExcelSheetColumnNumber
-{
+class ExcelSheetColumnNumber {
 public:
-    int titleToNumber(string columnTitle)
-    {
+    /**
+     * Complexities:
+     *   N - Size of `columnTitle`
+     *   - Time Complexity: O(N²)
+     *   - Space Complexity: O(1)
+     */
+    int titleToNumber(string columnTitle) {
         int length = columnTitle.size();
         int result = 0;
-        for (int i = 0; i < length; i++)
-        {
+
+        for (int i = 0; i < length; i++) {
             result += ((int)columnTitle[length - i - 1] - 64) * pow(26, i);
         }
+
         return result;
     }
 
-    // Best Solution
-    int bestSolution(string columnTitle)
-    {
+
+    // Solution
+    /**
+     * Complexities:
+     *   N - Size of `columnTitle`
+     *   - Time Complexity: O(N)
+     *   - Space Complexity: O(1)
+     */
+    int solution(string columnTitle) {
         int result = 0;
-        for (char c : columnTitle)
-        {
+
+        for (char c : columnTitle) {
             int d = c - 'A' + 1;
             result = result * 26 + d;
         }
+
         return result;
     }
 };
