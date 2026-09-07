440bbee7771929dc7d99bc53d696f1bf504d37fa
hyungupark <hyungu.1215.park@gmail.com>
2026-09-08 01:48:04 +0900

    leetcode: "171_excel_sheet_column_number" add c solution

--- leetcode/algorithms/171_excel_sheet_column_number/main.c
+++ leetcode/algorithms/171_excel_sheet_column_number/main.c
@@ -1,35 +1,45 @@
 #include <string.h>
 #include <math.h>
 
-int titleToNumber(char *columnTitle)
-{
+/**
+ * Complexities:
+ *   N - Size of `columnTitle`
+ *   - Time Complexity: O(N²)
+ *   - Space Complexity: O(1)
+ */
+int titleToNumber(char* columnTitle) {
     int length = 0;
-    while (1)
-    {
-        if (columnTitle[length] == 0)
-        {
+    while (1) {
+        if (columnTitle[length] == 0) {
             break;
         }
         length++;
     }
 
     int result = 0;
-    for (int i = 0; i < length; i++)
-    {
+    for (int i = 0; i < length; i++) {
         result += ((int)columnTitle[length - i - 1] - 64) * pow(26, i);
     }
 
     return result;
 }
 
-// Best Solution
-int bestSolution(char *columnTitle)
-{
-    int output = 0;
-    for (int i = 0; i < strlen(columnTitle); i++)
-    {
-        output *= 26;
-        output += (columnTitle[i] - 'A' + 1);
+
+// Solution
+/**
+ * Complexities:
+ *   N - Size of `columnTitle`
+ *   - Time Complexity: O(N)
+ *   - Space Complexity: O(1)
+ */
+int solution(char* columnTitle) {
+    long long result = 0; 
+    
+    for (int i = 0; columnTitle[i] != '\0'; i++) {
+        int char_value = columnTitle[i] - 'A' + 1;
+        
+        result = result * 26 + char_value;
     }
-    return output;
+    
+    return (int)result;
 }
