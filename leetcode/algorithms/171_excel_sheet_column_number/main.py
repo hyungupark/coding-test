536485a70e7d553976fb0038e30d8459fb02e30c
hyungupark <hyungu.1215.park@gmail.com>
2026-09-10 01:06:20 +0900

    leetcode: "171_excel_sheet_column_number" add python solution

--- leetcode/algorithms/171_excel_sheet_column_number/main.py
+++ leetcode/algorithms/171_excel_sheet_column_number/main.py
@@ -1,4 +1,10 @@
-class Solution:
+class ExcelSheetColumnNumber:
+    """
+    # Complexities:
+    #   N - Size of `columnTitle`
+    #   - Time Complexity: O(N²)
+    #   - Space Complexity: O(1)
+    """
     def titleToNumber(self, columnTitle: str) -> int:
         result = 0
 
@@ -7,12 +13,20 @@ class Solution:
 
         return result
 
-    # Best Solution
-    def bestSolution(self, columnTitle: str) -> int:
-        ans, pos = 0, 0
-        for letter in reversed(columnTitle):
-            digit = ord(letter) - 64
-            ans += digit * 26**pos
-            pos += 1
 
-        return ans
+    # Solution
+    """
+    # Complexities:
+    #   N - Size of `columnTitle`
+    #   - Time Complexity: O(N²)
+    #   - Space Complexity: O(1)
+    """
+    def solution(self, columnTitle: str) -> int:
+        result = 0
+
+        for char in columnTitle:
+            val = ord(char) - ord('A') + 1
+
+            result = result * 26 + val
+
+        return result
