91d8e506a9ef8029186e76c041980f75125b631c
hyungupark <hyungu.1215.park@gmail.com>
2026-09-16 01:08:14 +0900

    leetcode: "190_reverse_bits" add javascript solution

--- leetcode/algorithms/190_reverse_bits/main.js
+++ leetcode/algorithms/190_reverse_bits/main.js
@@ -1,26 +1,43 @@
 /**
+ * Complexities:
+ *   - Time Complexity: O(1)
+ *   - Space Complexity: O(1)
+ */
+/**
  * @param {number} n - a positive integer
  * @return {number} - a positive integer
  */
 var reverseBits = function (n) {
-  n = n.toString(2).split('').reverse();
+  n = n.toString(2).split("").reverse();
 
   while (n.length < 32) {
-    n.push('0');
+    n.push("0");
   }
 
-  return parseInt(n.join(''), 2);
+  return parseInt(n.join(""), 2);
 };
 
-// Best Solution
-var bestSolution = function (n) {
-  var result = 0;
-  var count = 32;
 
-  while (count--) {
-    result *= 2;
-    result += n & 1;
-    n = n >> 1;
+// Solution
+/**
+ * Complexities:
+ *   - Time Complexity: O(1)
+ *   - Space Complexity: O(1)
+ */
+/**
+ * @param {number} n - a positive integer
+ * @return {number} - a positive integer
+ */
+var solution = function (n) {
+  let result = 0;
+
+  for (let i = 0; i < 32; i++) {
+    const bit = n & 1;
+
+    result = (result << 1) | bit;
+
+    n = n >>> 1;
   }
-  return result;
+
+  return result >>> 0;
 };
