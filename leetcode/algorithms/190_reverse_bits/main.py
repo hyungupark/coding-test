56d722a297dda3327dedfb1996a8a773724c220f
hyungupark <hyungu.1215.park@gmail.com>
2026-09-17 01:08:22 +0900

    leetcode: "190_reverse_bits" add python solution

--- leetcode/algorithms/190_reverse_bits/main.py
+++ leetcode/algorithms/190_reverse_bits/main.py
@@ -1,4 +1,9 @@
-class Solution:
+class ReverseBits:
+    """
+    # Complexities:
+    #   - Time Complexity: O(1)
+    #   - Space Complexity: O(1)
+    """
     def reverseBits(self, n: int) -> int:
         n = bin(n).replace("0b", "")[::-1]
 
@@ -7,10 +12,33 @@ class Solution:
 
         return int(n, 2)
 
-    # Best Solution
-    def bestSolution(self, n: int) -> int:
+
+    # Solution
+    """
+    # Solution 1
+    #
+    # Bitwise Operations
+    #
+    # Complexities:
+    #   - Time Complexity: O(1)
+    #   - Space Complexity: O(1)
+    """
+    def solution1(self, n: int) -> int:
         res = 0
+
         for _ in range(32):
-            res = (res << 1) + (n & 1)
+            res = (res << 1) | (n & 1)
+
             n >>= 1
+
         return res
+
+    """
+    # Solution 2
+    #
+    # Complexities:
+    #   - Time Complexity: O(1)
+    #   - Space Complexity: O(1)
+    """
+    def solution2(self, n: int) -> int:
+        return int('{:032b}'.format(n)[::-1], 2)
