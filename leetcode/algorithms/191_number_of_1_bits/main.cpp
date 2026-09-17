5c6ff0d1d54ffd3e2be10bdd356661070c27233a
hyungupark <hyungu.1215.park@gmail.com>
2026-09-18 01:06:54 +0900

    leetcode: "191_number_of_1_bits" add c++ solution

--- leetcode/algorithms/191_number_of_1_bits/main.cpp
+++ leetcode/algorithms/191_number_of_1_bits/main.cpp
@@ -1,14 +1,20 @@
-class NumberOf1Bits
-{
+#include <bitset>
+using namespace std;
+
+class NumberOf1Bits {
 public:
-    int hammingWeight(int n)
-    {
+    /**
+     * Bit Shift & Mask
+     *
+     * Complexities:
+     *   - Time Complexity: O(1)
+     *   - Space Complexity: O(1)
+     */
+    int hammingWeight(int n) {
         int count = 0;
 
-        for (size_t i = 0; i < 32; i++)
-        {
-            if (n & 1)
-            {
+        for (int i = 0; i < 32; i++) {
+            if (n & 1) {
                 count++;
             }
             n = n >> 1;
@@ -17,17 +23,38 @@ public:
         return count;
     }
 
-    // Best Solution
-    int bestSolution(int n)
-    {
-        int count = 0;
 
-        while (n)
-        {
+    // Solution
+    /**
+     * Solution 1
+     * 
+     * bitset
+     *
+     * Complexities:
+     *   - Time Complexity: O(1)
+     *   - Space Complexity: O(1)
+     */
+    int solution1(int n) {
+        return bitset<32>(n).count();
+    }
+
+    /**
+     * Solution 2
+     * 
+     * Brian Kernighan's Algorithm
+     *
+     * Complexities:
+     *   - Time Complexity: O(1)
+     *   - Space Complexity: O(1)
+     */
+    int solution2(int n) {
+        int count = 0;
+        
+        while (n > 0) {
             n &= (n - 1);
             count++;
         }
-
+        
         return count;
     }
 };
