c1be47eda089c970df0267c80e8fcc8bde432367
hyungupark <hyungu.1215.park@gmail.com>
2026-09-26 03:24:02 +0900

    leetcode: "202_happy_number" add java solution

--- leetcode/algorithms/202_happy_number/main.java
+++ leetcode/algorithms/202_happy_number/main.java
@@ -4,29 +4,36 @@ import java.util.List;
 import java.util.Map;
 
 class HappyNumber {
-
+  /**
+   * Hash Map
+   *
+   * Complexities:
+   *   N - `n`
+   *   - Time Complexity: O(logᴺ)
+   *   - Space Complexity: O(logᴺ)
+   */
   public boolean isHappy(int n) {
     if (n <= 1) {
       return true;
     }
-    Map<Integer, Boolean> nMap = new HashMap<Integer, Boolean>();
+
+    Map<Integer, Boolean> nMap = new HashMap<>();
 
     while (n > 1) {
       if (nMap.get(n) != null && nMap.get(n)) {
         return false;
       }
+
       nMap.put(n, true);
-      List<Integer> nList = new ArrayList<Integer>();
+      List<Integer> nList = new ArrayList<>();
+
       for (int i = 0; i < String.valueOf(n).length(); i++) {
         nList.add(String.valueOf(n).charAt(i) - '0');
       }
-      n =
-        nList
+
+      n = nList
           .stream()
-          .reduce(
-            0,
-            (total_, current_) -> (int) (total_ + Math.pow(current_, 2))
-          );
+          .reduce(0, (total_, current_) -> (int) (total_ + Math.pow(current_, 2)));
 
       if (n == 1) {
         return true;
@@ -36,27 +43,37 @@ class HappyNumber {
     return false;
   }
 
-  // Best Solution
-  // Floyd's cycle-finding algorithm (Tortoise and the Hare algorithm)
-  public boolean bestSolution(int n) {
-    int s = n, f = n; // slow , fast
 
-    do {
-      s = compute(s); // slow computes only once
-      f = compute(compute(f)); // fast computes 2 times
+  // Solution
+  /**
+   * Floyd's Cycle-Finding Algorithm
+   *
+   * Complexities:
+   *   N - `n`
+   *   - Time Complexity: O(logᴺ)
+   *   - Space Complexity: O(1)
+   */
+  private int getNext(int n) {
+    int totalSum = 0;
 
-      if (s == 1) return true; // if we found 1 then happy indeed !!!
-    } while (s != f); // else at some point they will meet in the cycle
+    while (n > 0) {
+      int digit = n % 10;
+      n = n / 10;
+      totalSum += digit * digit;
+    }
 
-    return false; // it's a cycle , not happy at all !!!
+    return totalSum;
   }
 
-  public int compute(int n) {
-    int s = 0;
-    while (n != 0) {
-      s += (n % 10) * (n % 10);
-      n = n / 10;
+  public boolean solution(int n) {
+    int slow = n;
+    int fast = getNext(n);
+
+    while (fast != 1 && slow != fast) {
+      slow = getNext(slow);
+      fast = getNext(getNext(fast));
     }
-    return s;
+
+    return fast == 1;
   }
 }
